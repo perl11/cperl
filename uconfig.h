05c229f462477479448ccb11da507fe2644d4913
Reini Urban <rurban@cpanel.net>
2015-03-16 18:04:01 +0100

    metaconf: add dl extensions and path_sep to config.h
    New bool variable dl_so_eq_ext
    Add PATH_SEP, DLEXT, DLSO, DLSRC and DL_SO_EQ_EXT to config.h

--- uconfig.h
+++ uconfig.h
@@ -4779,6 +4779,33 @@
  */
 #define LDLIBPTHNAME 	""		/**/
 
+/* PATH_SEP:
+ *	This symbol indicates the character used to separate elements
+ *      in the command shell search PATH.
+ */
+#define PATH_SEP 	":"		/**/
+
+/* DLEXT:
+ *	This symbol indicates the extension that is to be used for the
+ *	dynamically loaded modules that perl generates. The "." is included.
+ */
+/* DLSO:
+ *	This variable holds the extension used to identify shared libraries
+ *	(also known as shared objects) to be linked on the system. Usually set to '.so'.
+ *      E.g. On darwin "dylib" vs. "bundle" for DLEXT.
+ */
+/* DLSRC:
+ *	This variable contains the name of the dynamic loading file that
+	will be used with the package.
+ */
+/* DL_SO_EQ_EXT:
+ *	This variable is defined if DLEXT equals DLSO.
+ */
+#define DLEXT 	".so"		/**/
+#define DLSO 	".so"			/**/
+#define DLSRC 	"dl_none.xs"		/**/
+#define DL_SO_EQ_EXT 		/**/
+
 /* PERL_PRIfldbl:
  *	This symbol, if defined, contains the string used by stdio to
  *	format long doubles (format 'f') for output.
@@ -5194,6 +5221,6 @@
 #endif
 
 /* Generated from:
- * f25e973500e13064dd1d7494049cfd5ee077744a417b8bbbe35b532ced263884 config_h.SH
- * 3fa3c2f2b3a2d56ac9c8825091dd8eb07825b5e7e9bc5b238d0e0d8bdca27c4d uconfig.sh
+ * 8863582d1c1d676c9f285e98d7c87846ef65a244eeb536af569cd4a8f45515ab config_h.SH
+ * 3aef1b9d110da2bbe0365d9fd46aafd1df315fc76a2b330d2295ded09b7b5fb5 uconfig.sh
  * ex: set ro: */
