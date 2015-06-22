8b9e07f47d20c5571df5dc431896295da987eeff
Karl Williamson <khw@cpan.org>
2015-06-21 21:38:32 -0600

    There are no folds to multiple chars in early Unicode versions
    Several places require special handling because of this, notably for the
    lowercase Sharp S, but not in Unicodes before 3.0.1

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -99521,7 +99521,7 @@ static const UV XPosixXDigit_invlist[] = { /* for EBCDIC POSIX-BC */
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * e3e2bd4d043645d3a9b00b857fe6182beeb5acd9261735b37440b7e942baf96a lib/unicore/mktables
+ * 8cfe52f570a42c07c5bcbd08472f5e1013e0759f90f1913349aa586eee421fd5 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * c8795af00edb83679f98e15d7aa0a27b2ff489655f7b5fbb24024f9ee07afbda regen/mk_invlists.pl
