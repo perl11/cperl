cece7c7ce2227a2f13f0aac4a9c911c73d6d076f
Karl Williamson <khw@cpan.org>
2015-06-26 11:50:18 -0600

    mktables: Allow strictly named map tables
    There are several types of tables generated by mktables.  Most are
    binary (match) tables, but another class is mapping tables.  The names
    for these may be loosely matched, but until this commit only the match
    tables could have strict matching applied.
    
    Strict matching is used for certain table names where loose could be
    ambiguous, and for all names that aren't to be used by anything except
    the perl core.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * eb7d4339e25dee3af61fd18be30e18e51e5451c026c5b84efc9058ef1c87589f lib/unicore/mktables
+ * 3e001a53770c55a905ebf1a8a43dc3cf9bda9d385bb2ee4e52891d0a0342d315 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 0f0028f3630298a065a489312dc6fb21e30d41a245a1990bf50690a097cb07d1 regen/regcharclass.pl
