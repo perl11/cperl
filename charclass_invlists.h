748bd9f4214266b4400bda88e2d017fc8964e409
Karl Williamson <khw@cpan.org>
2015-06-26 12:12:16 -0600

    mktables: Add true/false aliases in early Unicodes
    Before Unicode 5.0, the aliases for True and False were just assumed,
    and not specifically mentioned.  This commit adds them for those early
    releases.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -99521,7 +99521,7 @@ static const UV XPosixXDigit_invlist[] = { /* for EBCDIC POSIX-BC */
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 3e001a53770c55a905ebf1a8a43dc3cf9bda9d385bb2ee4e52891d0a0342d315 lib/unicore/mktables
+ * 856878fed22c739eda85eee35b6c628aed852676ff2289d1290039d3215884af lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * c8795af00edb83679f98e15d7aa0a27b2ff489655f7b5fbb24024f9ee07afbda regen/mk_invlists.pl
