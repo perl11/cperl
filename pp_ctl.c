2a262f7fa85eef89f9fc7995181a5b8c8e9cf4dd
David Mitchell <davem@iabyn.com>
2015-06-29 11:36:27 +0100

    document unrolled PUSHSUB/POPSUB
    some places in the code do some or all of the same activities as
    PUSHSUB() and POPSUB(). Add comments in those places pointing out that
    it's a partially unrolled PUSH/POPSUB(). This will also help if someone
    greps the code looking for all the places in core that supposedly does
    a PUSH/POPSUB.

--- pp_ctl.c
+++ pp_ctl.c
@@ -2742,6 +2742,9 @@ PP(pp_goto)
 		SvREFCNT_dec(cv);
 		DIE(aTHX_ "Can't goto subroutine from a sort sub (or similar callback)");
 	    }
+
+            /* partial unrolled POPSUB(): */
+
 	    if (CxTYPE(cx) == CXt_SUB && CxHASARGS(cx)) {
 		AV* av = cx->blk_sub.argarray;
 
@@ -2839,6 +2842,9 @@ PP(pp_goto)
 	    }
 	    else {
 		PADLIST * const padlist = CvPADLIST(cv);
+
+                /* partial unrolled PUSHSUB(): */
+
 		cx->blk_sub.cv = cv;
 		cx->blk_sub.olddepth = CvDEPTH(cv);
 
