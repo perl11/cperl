b99ad1b3a63107ef432ff33143260f89f2158fb7
David Mitchell <davem@iabyn.com>
2015-07-11 22:13:51 +0100

    Eliminate ENTER/LEAVE from sub calls
    Every sub call is wrapped in an ENTER/LEAVE pair, which uses the next
    free slot on the scope stack to save and then restore the old value of
    PL_savestack_ix. Instead, store the old value in a new field in the
    context structure, old_savestack_ix. This is quicker and simpler.
    
    Not that we keep the ENTER/LEAVE for XS sub calls, as they don't push a
    context frame, and so have nowhere else to remember PL_savestack_ix.
    
    As a side-effect, this commit fixes a TODO test in t/op/sub.t,
    which was related to dying while popping a context, then re-popping that
    context. For the second pop, the scopestack has since been overwritten
    and so too much was getting popped from the savestack. Since we no longer
    use the scopestack, it's no longer an issue.

--- pp_hot.c
+++ pp_hot.c
@@ -3440,7 +3440,6 @@ PP(pp_leavesub)
     }
     PUTBACK;
 
-    LEAVE;
     POPSUB(cx,sv);	/* Stack values are safe: release CV and @_ ... */
     cxstack_ix--;
     PL_curpm = newpm;	/* ... and pop $1 et al */
@@ -3480,6 +3479,7 @@ PP(pp_entersub)
     PERL_CONTEXT *cx;
     I32 gimme;
     const bool hasargs = (PL_op->op_flags & OPf_STACKED) != 0;
+    I32 old_savestack_ix;
 
     if (UNLIKELY(!sv))
 	DIE(aTHX_ "Not a CODE reference");
@@ -3493,7 +3493,7 @@ PP(pp_entersub)
                 cv = sv_2cv(sv, &stash, &gv, 0);
             }
             if (!cv) {
-                ENTER;
+                old_savestack_ix = PL_savestack_ix;
                 goto try_autoload;
             }
             break;
@@ -3556,7 +3556,13 @@ PP(pp_entersub)
         return PL_op->op_ppaddr(aTHX);
     }
 
-    ENTER;
+    /* At this point we want to save PL_savestack_ix, either by doing a
+     * PUSHSUB, or for XS, doing an ENTER. But we don't yet know the final
+     * CV we will be using (so we don't know whether its XS, so we can't
+     * PUSHSUB or ENTER yet), and determining cv may itself push stuff on
+     * the save stack. So remember where we are currently on the save
+     * stack, and later update the CX or scopestack entry accordingly. */
+    old_savestack_ix = PL_savestack_ix;
 
     /* these two fields are in a union. If they ever become separate,
      * we have to test for both of them being null below */
@@ -3652,8 +3658,9 @@ PP(pp_entersub)
 
 	PUSHBLOCK(cx, CXt_SUB, MARK);
 	PUSHSUB(cx);
-
 	cx->blk_sub.retop = PL_op->op_next;
+        cx->blk_sub.old_savestack_ix = old_savestack_ix;
+
 	if (UNLIKELY((depth = ++CvDEPTH(cv)) >= 2)) {
 	    PERL_STACK_OVERFLOW_CHECK();
 	    pad_push(padlist, depth);
@@ -3814,6 +3821,10 @@ PP(pp_enterxssub)
     {
 	SSize_t markix = TOPMARK;
         I32 gimme;
+
+        ENTER;
+        /* pretend we did the ENTER earlier */
+	PL_scopestack[PL_scopestack_ix - 1] = old_savestack_ix;
 
 	SAVETMPS;
 	PUTBACK;
