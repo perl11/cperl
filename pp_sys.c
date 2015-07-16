ef2fd1d21b613d7980c369f45f9d2dfaf2b35766
David Mitchell <davem@iabyn.com>
2015-07-16 14:44:06 +0100

    CXt_FORMAT: save ss_ix and tmps_floor in CX struct
    To match the recent work in this branch on CXt_SUB, make CXt_FORMAT save
    PL_savestack_ix and PL_tmps_floor in the context structure rather than
    doing ENTER/SAVETMPS. It's more efficient and more consistent.

--- pp_sys.c
+++ pp_sys.c
@@ -1380,9 +1380,6 @@ S_doform(pTHX_ CV *cv, GV *gv, OP *retop)
     if (CvCLONE(cv))
 	cv = MUTABLE_CV(sv_2mortal(MUTABLE_SV(cv_clone(cv))));
 
-    ENTER;
-    SAVETMPS;
-
     PUSHBLOCK(cx, CXt_FORMAT, PL_stack_sp);
     PUSHFORMAT(cx, retop);
     if (CvDEPTH(cv) >= 2) {
@@ -1523,7 +1520,6 @@ PP(pp_leavewrite)
     retop = cx->blk_sub.retop;
     POPFORMAT(cx);
     SP = newsp; /* ignore retval of formline */
-    LEAVE;
 
     if (is_return)
         /* XXX the semantics of doing 'return' in a format aren't documented.
