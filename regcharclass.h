97bd7b05a635c8ccd310a467831f8ef3a43e32e8
Karl Williamson <khw@cpan.org>
2015-07-23 21:04:03 -0600

    mktables: Rmv wrong code point in list
    This was a typo, but it hasn't done any harm as it is an unassigned code
    point, which gets weeded out.  Still it has no business being here.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 31fb0416cb7d4ff5eee8670c2194f7b81f9d01096867754b20a2069c4dd179ec lib/unicore/mktables
+ * 149a78ae869a3af6db9f5b6b346185052e661ecf88eafe5b89bb00b7dd0578f3 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 1c80f63db92ede11c1b80c61b7fb063ab6c63b7a8de43f2b3992487cd39a9323 regen/regcharclass.pl
