d0119b3f88335aca357f2c4f014a215296aff0d8
Karl Williamson <khw@cpan.org>
2015-07-24 22:43:55 -0600

    mktables: Two files came along later than mktables thinks
    NormTest.txt LBTest.txt were really in later releases.  This fixes that
    and moves each constructor to be in the respective release's group of
    files.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -99521,7 +99521,7 @@ static const UV XPosixXDigit_invlist[] = { /* for EBCDIC POSIX-BC */
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * bfb39a18c3307ab7c0b205cddaab85cfa637d6fc64e90af5953a4e5999bbb981 lib/unicore/mktables
+ * 40c4d398a8ee256d85bbed0f6dd401dbcb4758cce8c5275e02891ae55a9e1623 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * f1b8aa9b5418213a151ab3cac8ca85c556de235ef87582fd1864e84e99643a50 regen/mk_invlists.pl
