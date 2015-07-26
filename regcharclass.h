6a8943f56abc48c3be7061ae39c75cf6689c3d1c
Karl Williamson <khw@cpan.org>
2015-07-25 19:19:05 -0600

    mktables: Allow multiple eof handlers for input files
    The Input_file class is hereby extended so there can be more than one
    subroutine stacked to call when an EOF is encountered.  This new
    capability will be used in later commits.  There is no current need for
    any outside class to look at this, so that capability is removed.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 20e1522657eb0bd57f95efa9d71a9caa07e4605d5092ce8ce0ff3a4c69d197b0 lib/unicore/mktables
+ * 14f50d65db6246bc57d695b6a39ebbb9406f66707ce136fa9a2520b259511fc2 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 1c80f63db92ede11c1b80c61b7fb063ab6c63b7a8de43f2b3992487cd39a9323 regen/regcharclass.pl
