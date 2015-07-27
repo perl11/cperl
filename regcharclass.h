aa51e2ea453b369ba7750d3a56b6809ae6bdb42e
Karl Williamson <khw@cpan.org>
2015-07-26 21:30:07 -0600

    mktables: Add specifiable construction handler for Input_file class
    This object is for the files in the Unicode Character Database.  This
    commit adds the capability for the constructor new() to be passed a code
    reference to call after everything else is constructed, but before
    control is returned.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * cf6133e62476bceb39274d513b696e23955785459fe5bc469749a0415e42b2df lib/unicore/mktables
+ * 5a41aceb054fe90858e6cc52edf473d80e0842337c52d48e6e4f571cc0165491 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 1c80f63db92ede11c1b80c61b7fb063ab6c63b7a8de43f2b3992487cd39a9323 regen/regcharclass.pl
