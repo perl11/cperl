edf263d8fcc9ed78ab5e150b55903bfc7df45b64
Karl Williamson <khw@cpan.org>
2015-07-26 22:47:56 -0600

    mktables: Convert PropValueAliases.txt to use new early infrastructure
    This file is crucial to compiling perl these days.  This commit converts
    to use the new infrastructure for dealing with compiling Unicode
    releases prior to when this file was made available.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 3704ffb9e5cd40a863970436ed8257e9bf67c8f41d01bf3b047c636dfef6701c lib/unicore/mktables
+ * a357a2c9c3a68d6910a969ead89582ff98217a4504f714c42ab1062095e875c0 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 1c80f63db92ede11c1b80c61b7fb063ab6c63b7a8de43f2b3992487cd39a9323 regen/regcharclass.pl
