aafe241c01f114e49db63363ddf9acc0db9e45d7
Karl Williamson <khw@cpan.org>
2015-07-28 20:34:22 -0600

    mktables: improve LineBreak table for early Unicodes
    It turns out that the generated map files look better (even if
    functionally equivalent) if the default mapping is the one to the
    above-Unicode code points.  This was the only one that had it different.

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * ba277e35ae43bb57ca33897c22cc0195524c74d9135dd47c30059f1b687e9925 lib/unicore/mktables
+ * eae7279abdb9197306714d82d5c601058ef4456af97f45e1a48b7e1ba91cfee4 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 1c80f63db92ede11c1b80c61b7fb063ab6c63b7a8de43f2b3992487cd39a9323 regen/regcharclass.pl
