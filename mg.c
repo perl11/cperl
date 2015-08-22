8dcfd8a041229be7dc7c25bda60ae84df1b6328f
Andy Broad <andy@broad.ology.org.uk>
2015-08-22 09:08:30 -0400

    amigaos4: setenv magic should work with __amigaos4__
    The code was protected with AMIGAOS which was AmigaOS 3.

--- mg.c
+++ mg.c
@@ -1215,7 +1215,7 @@ Perl_magic_setenv(pTHX_ SV *sv, MAGIC *mg)
     }
 #endif
 
-#if !defined(OS2) && !defined(AMIGAOS) && !defined(WIN32) && !defined(MSDOS)
+#if !defined(OS2) && !defined(WIN32) && !defined(MSDOS)
 			    /* And you'll never guess what the dog had */
 			    /*   in its mouth... */
     if (TAINTING_get) {
@@ -1275,7 +1275,7 @@ Perl_magic_setenv(pTHX_ SV *sv, MAGIC *mg)
 	    }
 	}
     }
-#endif /* neither OS2 nor AMIGAOS nor WIN32 nor MSDOS */
+#endif /* neither OS2 nor WIN32 nor MSDOS */
 
     return 0;
 }
