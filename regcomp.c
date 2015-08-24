39a5beac1f12c6138ad1276954e4d3325211735f
Karl Williamson <khw@cpan.org>
2015-08-23 21:39:04 -0600

    PATCH: [perl #125805] Perl segfaults with a regex_sets error message
    This fix required an extra test of the return value of a function.
    
    (cherry picked from commit e7cce976d7dd1f4fda1f387d02c6403f43346e9c)

--- regcomp.c
+++ regcomp.c
@@ -13898,6 +13898,7 @@ redo_curchar:
     if (av_tindex(stack) < 0   /* Was empty */
         || ((final = av_pop(stack)) == NULL)
         || ! IS_OPERAND(final)
+        || SvTYPE(final) != SVt_INVLIST
         || av_tindex(stack) >= 0)  /* More left on stack */
     {
         SvREFCNT_dec(final);
