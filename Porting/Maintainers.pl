df0d761141e1454fccbdebc5a524c25620690031
Andy Broad <andy@broad.ology.org.uk>
2015-08-27 19:19:45 -0400

    amigaos4: Amiga extensions maintenance

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1538,6 +1538,8 @@ use File::Glob qw(:case);
 
     '_PERLLIB' => {
         'FILES'    => q[
+                ext/Amiga-ARexx/
+                ext/Amiga-Exec/
                 ext/B/
                 ext/Devel-Peek/
                 ext/DynaLoader/
