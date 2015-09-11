c1c250f6be56405cb29aa20b9fd3dd6fde07aeee
Karl Williamson <khw@cpan.org>
2015-09-10 22:31:39 -0600

    pods: Discourage use of 'In' prefix for Unicode Block property
    This changes perluniprops to not list the equivalent 'In' single form
    method of specifying the Block property, and to discourage its use.  The
    reason is that this is a Perl extension, the use of which is unstable.
    A future Unicode release could take over the 'In...' name for a new
    purpose, and perl would follow along, breaking the code that assumed the
    former meaning.  Unicode does not know about this Perl extension, and
    they wouldn't care if they did know.
    
    The reason I'm doing this now is that the latest Unicode version
    introduced some properties whose names begin with 'In', though no
    conflicts arose.  But it is clear that such conflicts could arise in the
    future.  So the documentation only is changed to warn people of this
    potential.
    
    perlunicode is update accordingly.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -99537,7 +99537,7 @@ static const UV XPosixXDigit_invlist[] = { /* for EBCDIC POSIX-BC */
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 9aeb4ec91dd7fba17a28c0f0adb878bb6143515bbdf106f32f77769303bad7cf lib/unicore/mktables
+ * f4ffb06dae7e966b478dcf0b0c3e965c11b26dcd5dc544a324cb075b0b4a5d1d lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * eeedd7b83f3da6af63fd0ad7c9c4ac8213c1157f9cf55173d19b891d09f942d1 regen/mk_invlists.pl
--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 9aeb4ec91dd7fba17a28c0f0adb878bb6143515bbdf106f32f77769303bad7cf lib/unicore/mktables
+ * f4ffb06dae7e966b478dcf0b0c3e965c11b26dcd5dc544a324cb075b0b4a5d1d lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * c6884f4d629f04d1316f3476cb1050b6a1b98ca30c903262955d4eae337c6b1e regen/charset_translations.pl
  * 1c80f63db92ede11c1b80c61b7fb063ab6c63b7a8de43f2b3992487cd39a9323 regen/regcharclass.pl
