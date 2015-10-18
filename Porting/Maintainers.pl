9728611b5c2ad84c7709d65f2a5d4e85b602a8ad
Matthew Horsfall <wolfsage@gmail.com>
2015-10-18 17:20:41 +0100

    Updates to Module::CoreList per RMG.
    (cherry picked from commit 003900e275f0c45375d4d02ffb0ac0fcd2eacfa2)

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -852,7 +852,7 @@ use File::Glob qw(:case);
     },
 
     'Module::CoreList' => {
-        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20150520.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Module-CoreList-5.20150720.tar.gz',
         'FILES'        => q[dist/Module-CoreList],
         # skip ending 'c' in numeric context on cperl
         'CUSTOMIZED'   => [ qw( lib/Module/CoreList.pm
