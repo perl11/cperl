Perl/perl5

commit 8cc95fdbe3687330bd987cea170494b4cd263d90

[inseparable changes from match from perl-5.003_97b to perl-5.003_97c]
 BUILD PROCESS

Subject: Fix syntax error in Configure comment(!)
From: Chip Salzenberg <chip@perl.com>
Files: Configure

Subject: For Solaris, if -DDEBUGGING, default to '-KPIC', not '-Kpic'
From: Chip Salzenberg <chip@perl.com>
Files: Configure

Subject: Fix usage of dXSUB_SYS, esp. in ExtUtils::Miniperl
From: Chip Salzenberg <chip@perl.com>
Files: dosish.h minimod.pl os2/os2ish.h plan9/plan9ish.h vms/vmsish.h

 CORE LANGUAGE CHANGES

Subject: Refine setgroups() behavior of C<$)>
From: Chip Salzenberg <chip@perl.com>
Files: mg.c pod/perldelta.pod pod/perlvar.pod

Subject: Forbid -[Mm] on #! line
From: Chip Salzenberg <chip@perl.com>
Files: pod/perldelta.pod pod/perldiag.pod toke.c

 CORE PORTABILITY

Subject: Special mkdir() for VMS
Date: Tue, 08 Apr 1997 12:33:56 -0400 (EDT)
From: Charles Bailey <bailey@hmivax.humgen.upenn.edu>
Files: dosish.h lib/ExtUtils/MM_Unix.pm lib/File/Path.pm os2/os2ish.h plan9/plan9ish.h pp_sys.c unixish.h vms/vms.c vms/vmsish.h
Msg-ID: 01IHGOXN6MZM0004K3@hmivax.humgen.upenn.edu

    (applied based on p5p patch as commit ebec1d4b2ca9c08d24035a369c7aa782aa058e66)

 DOCUMENTATION

Subject: IO::Socket doc fix
Date: Tue, 08 Apr 1997 15:30:43 -0400
From: Roderick Schertler <roderick@argon.org>
Files: ext/IO/lib/IO/Socket.pm
Msg-ID: 28383.860527843@eeyore.ibcinc.com

    (applied based on p5p patch as commit 4a51304ce091cb6d1ccc9f25e11753bd7d985b32)

 LIBRARY AND EXTENSIONS

Subject: Replace Class::Template with improved Class::Struct
From: Jim Miner <jfm@winternet.com>
Files: MANIFEST lib/Class/Struct.pm lib/Class/Template.pm lib/File/stat.pm lib/Net/hostent.pm lib/Net/netent.pm lib/Net/protoent.pm lib/Net/servent.pm lib/Time/gmtime.pm lib/Time/localtime.pm lib/Time/tm.pm lib/User/grent.pm lib/User/pwent.pm pod/perldelta.pod pod/perlfaq7.pod pod/perlmod.pod pod/perltoot.pod

Subject: MakeMaker pathname patch
Date: Wed, 9 Apr 1997 20:08:23 +0100

os2/os2ish.h
@@ -78,7 +78,7 @@ void Perl_OS2_init(char **);
 /* #define PERL_SYS_TERM() STMT_START {	\
     if (Perl_HAB_set) WinTerminate(Perl_hab);	} STMT_END */
 
-#define dXSUB_SYS int fake = OS2_XS_init()
+#define dXSUB_SYS OS2_XS_init()
 
 #ifdef PERL_IS_AOUT
 /* #  define HAS_FORK */
@@ -156,6 +156,7 @@ void *emx_realloc (void *, size_t);
 #define Stat(fname,bufptr) os2_stat((fname),(bufptr))
 #define Fstat(fd,bufptr)   fstat((fd),(bufptr))
 #define Fflush(fp)         fflush(fp)
+#define Mkdir(path,mode)   mkdir((path),(mode))
 
 #undef S_IFBLK
 #undef S_ISBLK
@@ -167,6 +168,7 @@ void *emx_realloc (void *, size_t);
 #define Stat(fname,bufptr) stat((fname),(bufptr))
 #define Fstat(fd,bufptr)   fstat((fd),(bufptr))
 #define Fflush(fp)         fflush(fp)
+#define Mkdir(path,mode)   mkdir((path),(mode))
 
 #endif
 

Configure
@@ -4413,8 +4413,12 @@ EOM
 		'') case "$osname" in
 			hpux)	dflt='+z' ;;
 			next)	dflt='none' ;;
-			solaris|svr4*|esix*) dflt='-Kpic' ;;
-			irix*)  dflt='-KPIC' ;;
+			svr4*|esix*)	dflt='-Kpic' ;;
+			irix*)	dflt='-KPIC' ;;
+			solaris) case "$ccflags" in
+				*-DDEBUGGING*)	dflt='-KPIC' ;;
+				*)		dflt='-Kpic' ;;
+				esac ;;
 			sunos)	dflt='-pic' ;;
 			*)	dflt='none' ;;
 		    esac ;;
@@ -8495,7 +8499,7 @@ eval $inlibc
 set setgroups d_setgrps
 eval $inlibc
 
-: Find type of 2nd arg to getgroups (and setgroups)
+: Find type of 2nd arg to 'getgroups()' and 'setgroups()'
 echo " "
 case "$d_getgrps$d_setgrps" in
 *define*)

MANIFEST
@@ -319,7 +319,7 @@ lib/CPAN.pm		Interface to Comprehensive Perl Archive Network
 lib/CPAN/FirstTime.pm	Utility for creating CPAN config files
 lib/CPAN/Nox.pm		Runs CPAN while avoiding compiled extensions
 lib/Carp.pm		Error message base class
-lib/Class/Template.pm	Structure/member template builder; makes nested types
+lib/Class/Struct.pm	Declare struct-like datatypes as Perl classes
 lib/Cwd.pm		Various cwd routines (getcwd, fastcwd, chdir)
 lib/Devel/SelfStubber.pm Generate stubs for SelfLoader.pm
 lib/DirHandle.pm	like FileHandle only for directories
@@ -391,7 +391,7 @@ lib/Tie/SubstrHash.pm	Compact hash for known key, value and table size
 lib/Time/Local.pm	Reverse translation of localtime, gmtime
 lib/Time/gmtime.pm	By-name interface to Perl's built-in gmtime
 lib/Time/localtime.pm	By-name interface to Perl's built-in localtime
-lib/Time/tm.pm		Internal oject for Time::{gm,local}time
+lib/Time/tm.pm		Internal object for Time::{gm,local}time
 lib/UNIVERSAL.pm	Base class for ALL classes
 lib/User/grent.pm	By-name interface to Perl's built-in getgr*
 lib/User/pwent.pm	By-name interface to Perl's built-in getpw*

dosish.h
@@ -18,7 +18,7 @@ void Perl_DJGPP_init();
 #endif	/* DJGPP */
 
 #define PERL_SYS_TERM()
-#define dXSUB_SYS int dummy
+#define dXSUB_SYS
 #define TMPPATH "plXXXXXX"
 
 #ifdef WIN32
@@ -86,6 +86,7 @@ void Perl_DJGPP_init();
 
 #define Fstat(fd,bufptr)   fstat((fd),(bufptr))
 #define Fflush(fp)         fflush(fp)
+#define Mkdir(path,mode)   mkdir((path),(mode))
 
 #ifndef WIN32
 #  define Stat(fname,bufptr) stat((fname),(bufptr))

ext/IO/lib/IO/Socket.pm
@@ -118,7 +118,7 @@ use Exporter;
 
 @ISA = qw(IO::Handle);
 
-$VERSION = "1.16";
+$VERSION = "1.1602";
 
 sub import {
     my $pkg = shift;
@@ -157,7 +157,7 @@ sub configure {
 	unless ref($fh) eq "IO::Socket";
 
     bless($fh, $domain2pkg[$domain]);
-    $fh->configure;
+    $fh->configure($arg);
 }
 
 sub socket {
@@ -417,7 +417,7 @@ the service name.
 Examples:
 
    $sock = IO::Socket::INET->new(PeerAddr => 'www.perl.org',
-                                 PeerPort => http(80),
+                                 PeerPort => 'http(80)',
                                  Proto    => 'tcp');
 
    $sock = IO::Socket::INET->new(PeerAddr => 'localhost:smtp(25)');

lib/Class/Struct.pm
@@ -1,0 +1,482 @@
+package Class::Struct;
+
+## See POD after __END__
+
+require 5.002;
+
+use strict;
+use vars qw(@ISA @EXPORT);
+
+use Carp;
+
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT = qw(struct);
+
+## Tested on 5.002 and 5.003 without class membership tests:
+my $CHECK_CLASS_MEMBERSHIP = ($] >= 5.003_95);
+
+my $print = 0;
+sub printem {
+    if (@_) { $print = shift }
+    else    { $print++ }
+}
+
+{
+    package Class::Struct::Tie_ISA;
+
+    sub TIEARRAY {
+        my $class = shift;
+        return bless [], $class;
+    }
+
+    sub STORE {
+        my ($self, $index, $value) = @_;
+        Class::Struct::_subclass_error();
+    }
+
+    sub FETCH {
+        my ($self, $index) = @_;
+        $self->[$index];
+    }
+
+    sub DESTROY { }
+}
+
+sub struct {
+
+    # Determine parameter list structure, one of:
+    #   struct( class => [ element-list ])
+    #   struct( class => { element-list })
+    #   struct( element-list )
+    # Latter form assumes current package name as struct name.
+
+    my ($class, @decls);
+    my $base_type = ref $_[1];
+    if ( $base_type eq 'HASH' ) {
+        $class = shift;
+        @decls = %{shift()};
+        _usage_error() if @_;
+    }
+    elsif ( $base_type eq 'ARRAY' ) {
+        $class = shift;
+        @decls = @{shift()};
+        _usage_error() if @_;
+    }
+    else {
+        $base_type = 'ARRAY';
+        $class = (caller())[0];
+        @decls = @_;
+    }
+    _usage_error() if @decls % 2 == 1;
+
+    # Ensure we are not, and will not be, a subclass.
+
+    my $isa = do {
+        no strict 'refs';
+        \@{$class . '::ISA'};
+    };
+    _subclass_error() if @$isa;
+    tie @$isa, 'Class::Struct::Tie_ISA';
+
+    # Create constructor.
+
+    croak "function 'new' already defined in package $class"
+        if do { no strict 'refs'; defined &{$class . "::new"} };
+
+    my @methods = ();
+    my %refs = ();
+    my %arrays = ();
+    my %hashes = ();
+    my %classes = ();
+    my $got_class = 0;
+    my $out = '';
+
+    $out = "{\n  package $class;\n  use Carp;\n  sub new {\n";
+
+    my $cnt = 0;
+    my $idx = 0;
+    my( $cmt, $name, $type, $elem );
+
+    if( $base_type eq 'HASH' ){
+        $out .= "    my(\$r) = {};\n";
+        $cmt = '';
+    }
+    elsif( $base_type eq 'ARRAY' ){
+        $out .= "    my(\$r) = [];\n";
+    }
+    while( $idx < @decls ){
+        $name = $decls[$idx];
+        $type = $decls[$idx+1];
+        push( @methods, $name );
+        if( $base_type eq 'HASH' ){
+            $elem = "{'$name'}";
+        }
+        elsif( $base_type eq 'ARRAY' ){
+            $elem = "[$cnt]";
+            ++$cnt;
+            $cmt = " # $name";
+        }
+        if( $type =~ /^\*(.)/ ){
+            $refs{$name}++;
+            $type = $1;
+        }
+        if( $type eq '@' ){
+            $out .= "    \$r->$elem = [];$cmt\n";
+            $arrays{$name}++;
+        }
+        elsif( $type eq '%' ){
+            $out .= "    \$r->$elem = {};$cmt\n";
+            $hashes{$name}++;
+        }
+        elsif ( $type eq '$') {
+            $out .= "    \$r->$elem = undef;$cmt\n";
+        }
+        elsif( $type =~ /^\w+(?:::\w+)*$/ ){
+            $out .= "    \$r->$elem = '${type}'->new();$cmt\n";
+            $classes{$name} = $type;
+            $got_class = 1;
+        }
+        else{
+            croak "'$type' is not a valid struct element type";
+        }
+        $idx += 2;
+    }
+    $out .= "    bless \$r;\n  }\n";
+
+    # Create accessor methods.
+
+    if ( $got_class && $CHECK_CLASS_MEMBERSHIP ) {
+        $out .= "  use UNIVERSAL;\n";
+    }
+    my( $pre, $pst, $sel );
+    $cnt = 0;
+    foreach $name (@methods){
+        if ( do { no strict 'refs'; defined &{$class . "::$name"} } ) {
+            carp "function '$name' already defined, overrides struct accessor method"
+                if $^W;
+        }
+        else {
+            $pre = $pst = $cmt = $sel = '';
+            if( defined $refs{$name} ){
+                $pre = "\\(";
+                $pst = ")";
+                $cmt = " # returns ref";
+            }
+            $out .= "  sub $name {$cmt\n    my \$r = shift;\n";
+            if( $base_type eq 'ARRAY' ){
+                $elem = "[$cnt]";
+                ++$cnt;
+            }
+            elsif( $base_type eq 'HASH' ){
+                $elem = "{'$name'}";
+            }
+            if( defined $arrays{$name} ){
+                $out .= "    my \$i;\n";
+                $out .= "    \@_ ? (\$i = shift) : return $pre\$r->$elem$pst;\n";
+                $sel = "->[\$i]";
+            }
+            elsif( defined $hashes{$name} ){
+                $out .= "    my \$i;\n";
+                $out .= "    \@_ ? (\$i = shift) : return $pre\$r->$elem$pst;\n";
+                $sel = "->{\$i}";
+            }
+            elsif( defined $classes{$name} ){
+                if ( $CHECK_CLASS_MEMBERSHIP ) {
+                    $out .= "    croak '$name argument is wrong class' if \@_ && ! UNIVERSAL::isa(\$_[0], '$type');\n";
+                }
+            }
+            $out .= "    croak 'Too many args to $name' if \@_ > 1;\n";
+            $out .= "    \@_ ? ($pre\$r->$elem$sel = shift$pst) : $pre\$r->$elem$sel$pst;\n";
+            $out .= "  }\n";
+        }
+    }
+    $out .= "}\n1;\n";
+
+    print $out if $print;
+    my $result = eval $out;
+    carp $@ if $@;
+}
+
+sub _usage_error {
+    confess "struct usage error";
+}
+
+sub _subclass_error {
+    croak 'struct class cannot be a subclass (@ISA not allowed)';
+}
+
+1; # for require
+
+
+__END__
+
+=head1 NAME
+
+Class::Struct - declare struct-like datatypes as Perl classes
+
+=head1 SYNOPSIS
+
+    use Class::Struct;
+            # declare struct, based on array:
+    struct( CLASS_NAME => [ ELEMENT_NAME => ELEMENT_TYPE, ... ]);
+            # declare struct, based on hash:
+    struct( CLASS_NAME => { ELEMENT_NAME => ELEMENT_TYPE, ... });
+
+    package CLASS_NAME;
+    use Class::Struct;
+            # declare struct, based on array, implicit class name:
+    struct( ELEMENT_NAME => ELEMENT_TYPE, ... );
+
+
+    package Myobj;
+    use Class::Struct;
+            # declare struct with four types of elements:
+    struct( s => '$', a => '@', h => '%', c => 'My_Other_Class' );
+
+    $obj = new Myobj;               # constructor
+
+                                    # scalar type accessor:
+    $element_value = $obj->s;           # element value
+    $obj->s('new value');               # assign to element
+
+                                    # array type accessor:
+    $ary_ref = $obj->a;                 # reference to whole array
+    $ary_element_value = $obj->a(2);    # array element value
+    $obj->a(2, 'new value');            # assign to array element
+
+                                    # hash type accessor:
+    $hash_ref = $obj->h;                # reference to whole hash
+    $hash_element_value = $obj->h('x'); # hash element value
+    $obj->h('x', 'new value');        # assign to hash element
+
+                                    # class type accessor:
+    $element_value = $obj->c;           # object reference
+    $obj->c->method(...);               # call method of object
+    $obj->c(new My_Other_Class);        # assign a new object
+
+
+=head1 DESCRIPTION
+
+C<Class::Struct> exports a single function, C<struct>.
+Given a list of element names and types, and optionally
+a class name, C<struct> creates a Perl 5 class that implements
+a "struct-like" data structure.
+
+The new class is given a constructor method, C<new>, for creating
+struct objects.
+
+Each element in the struct data has an accessor method, which is
+used to assign to the element and to fetch its value.  The
+default accessor can be overridden by declaring a C<sub> of the
+same name in the package.  (See Example 2.)
+
+Each element's type can be scalar, array, hash, or class.
+
+
+=head2 The C<struct()> function
+
+The C<struct> function has three forms of parameter-list.
+
+    struct( CLASS_NAME => [ ELEMENT_LIST ]);
+    struct( CLASS_NAME => { ELEMENT_LIST });
+    struct( ELEMENT_LIST );
+
+The first and second forms explicitly identify the name of the
+class being created.  The third form assumes the current package
+name as the class name.
+
+An object of a class created by the first and third forms is
+based on an array, whereas an object of a class created by the
+second form is based on a hash. The array-based forms will be
+somewhat faster and smaller; the hash-based forms are more
+flexible.
+
+The class created by C<struct> must not be a subclass of another
+class other than C<UNIVERSAL>.
+
+A function named C<new> must not be explicitly defined in a class
+created by C<struct>.
+
+The I<ELEMENT_LIST> has the form
+
+    NAME => TYPE, ...
+
+Each name-type pair declares one element of the struct. Each
+element name will be defined as an accessor method unless a
+method by that name is explicitly defined; in the latter case, a
+warning is issued if the warning flag (-w) is set.
+
+
+=head2 Element Types and Accessor Methods
+
+The four element types -- scalar, array, hash, and class -- are
+represented by strings -- C<'$'>, C<'@'>, C<'%'>, and a class name --
+optionally preceded by a C<'*'>.
+
+The accessor method provided by C<struct> for an element depends
+on the declared type of the element.
+
+=over
+
+=item Scalar (C<'$'> or C<'*$'>)
+
+The element is a scalar, and is initialized to C<undef>.
+
+The accessor's argument, if any, is assigned to the element.
+
+If the element type is C<'$'>, the value of the element (after
+assignment) is returned. If the element type is C<'*$'>, a reference
+to the element is returned.
+
+=item Array (C<'@'> or C<'*@'>)
+
+The element is an array, initialized to C<()>.
+
+With no argument, the accessor returns a reference to the
+element's whole array.
+
+With one or two arguments, the first argument is an index
+specifying one element of the array; the second argument, if
+present, is assigned to the array element.  If the element type
+is C<'@'>, the accessor returns the array element value.  If the
+element type is C<'*@'>, a reference to the array element is
+returned.
+
+=item Hash (C<'%'> or C<'*%'>)
+
+The element is a hash, initialized to C<()>.
+
+With no argument, the accessor returns a reference to the
+element's whole hash.
+
+With one or two arguments, the first argument is a key specifying
+one element of the hash; the second argument, if present, is
+assigned to the hash element.  If the element type is C<'%'>, the
+accessor returns the hash element value.  If the element type is
+C<'*%'>, a reference to the hash element is returned.
+
+=item Class (C<'Class_Name'> or C<'*Class_Name'>)
+
+The element's value must be a reference blessed to the named
+class or to one of its subclasses. The element is initialized to
+the result of calling the C<new> constructor of the named class.
+
+The accessor's argument, if any, is assigned to the element. The
+accessor will C<croak> if this is not an appropriate object
+reference.
+
+If the element type does not start with a C<'*'>, the accessor
+returns the element value (after assignment). If the element type
+starts with a C<'*'>, a reference to the element itself is returned.
+
+=back
+
+=head1 EXAMPLES
+
+=over
+
+=item Example 1
+
+Giving a struct element a class type that is also a struct is how
+structs are nested.  Here, C<timeval> represents a time (seconds and
+microseconds), and C<rusage> has two elements, each of which is of
+type C<timeval>.
+
+    use Class::Struct;
+
+    struct( rusage => {
+        ru_utime => timeval,  # seconds
+        ru_stime => timeval,  # microseconds
+    });
+
+    struct( timeval => [
+        tv_secs  => '$',
+        tv_usecs => '$',
+    ]);
+
+        # create an object:
+    my $t = new rusage;
+    	# $t->ru_utime and $t->ru_stime are objects of type timeval.
+
+        # set $t->ru_utime to 100.0 sec and $t->ru_stime to 5.0 sec.
+    $t->ru_utime->tv_secs(100);
+    $t->ru_utime->tv_usecs(0);
+    $t->ru_stime->tv_secs(5);
+    $t->ru_stime->tv_usecs(0);
+
+
+=item Example 2
+
+An accessor function can be redefined in order to provide
+additional checking of values, etc.  Here, we want the C<count>
+element always to be nonnegative, so we redefine the C<count>
+accessor accordingly.
+
+    package MyObj;
+    use Class::Struct;
+
+    		# declare the struct
+    struct ( 'MyObj', { count => '$', stuff => '%' } );
+
+    		# override the default accessor method for 'count'
+    sub count {
+        my $self = shift;
+        if ( @_ ) {
+            die 'count must be nonnegative' if $_[0] < 0;
+            $self->{'count'} = shift;
+            warn "Too many args to count" if @_;
+        }
+        return $self->{'count'};
+    }
+
+    package main;
+    $x = new MyObj;
+    print "\$x->count(5) = ", $x->count(5), "\n";
+                            # prints '$x->count(5) = 5'
+
+    print "\$x->count = ", $x->count, "\n";
+                            # prints '$x->count = 5'
+
+    print "\$x->count(-5) = ", $x->count(-5), "\n";
+                            # dies due to negative argument!
+
+
+=head1 Author and Modification History
+
+
+Renamed to C<Class::Struct> and modified by Jim Miner, 1997-04-02.
+
+    members() function removed.
+    Documentation corrected and extended.
+    Use of struct() in a subclass prohibited.
+    User definition of accessor allowed.
+    Treatment of '*' in element types corrected.
+    Treatment of classes as element types corrected.
+    Class name to struct() made optional.
+    Diagnostic checks added.
+
+
+Originally C<Class::Template> by Dean Roehrich.
+
+    # Template.pm   --- struct/member template builder
+    #   12mar95
+    #   Dean Roehrich
+    #
+    # changes/bugs fixed since 28nov94 version:
+    #  - podified
+    # changes/bugs fixed since 21nov94 version:
+    #  - Fixed examples.
+    # changes/bugs fixed since 02sep94 version:
+    #  - Moved to Class::Template.
+    # changes/bugs fixed since 20feb94 version:
+    #  - Updated to be a more proper module.
+    #  - Added "use strict".
+    #  - Bug in build_methods, was using @var when @$var needed.
+    #  - Now using my() rather than local().
+    #
+    # Uses perl5 classes to create nested data types.
+    # This is offered as one implementation of Tom Christiansen's "structs.pl"
+    # idea.
+
+=cut

lib/ExtUtils/MM_Unix.pm
@@ -662,11 +662,17 @@ sub dir_target {
 # too often :)
 
     my($self,@dirs) = @_;
-    my(@m,$dir);
+    my(@m,$dir,$targdir);
     foreach $dir (@dirs) {
 	my($src) = $self->catfile($self->{PERL_INC},'perl.h');
 	my($targ) = $self->catfile($dir,'.exists');
-	my($targdir) = dirname($targ); # Necessary because catfile may have adapted syntax of $dir to target OS
+	# catfile may have adapted syntax of $dir to target OS, so...
+	if ($Is_VMS) { # Just remove file name; dirspec is often in macro
+	    ($targdir = $targ) =~ s:/?\.exists$::;
+	}
+	else { # while elsewhere we expect to see the dir separator in $targ
+	    $targdir = dirname($targ);
+	}
 	next if $self->{DIR_TARGET}{$self}{$targdir}++;
 	push @m, qq{
 $targ :: $src
@@ -1350,7 +1356,7 @@ sub init_dirscan {	# --- File and Directory Lists (.xs .pm .pod etc)
 
 Initializes NAME, FULLEXT, BASEEXT, PARENT_NAME, DLBASE, PERL_SRC,
 PERL_LIB, PERL_ARCHLIB, PERL_INC, INSTALLDIRS, INST_*, INSTALL*,
-PREFIX, CONFIG, AR, AR_STATIC_ARGS, LD, OBJ_EXT, LIB_EXT, MAP_TARGET,
+PREFIX, CONFIG, AR, AR_STATIC_ARGS, LD, OBJ_EXT, LIB_EXT, EXE_EXT, MAP_TARGET,
 LIBPERL_A, VERSION_FROM, VERSION, DISTNAME, VERSION_SYM.
 
 =cut
@@ -1539,7 +1545,7 @@ usually solves this kind of problem.
     # all the installation path variables to literally $(PREFIX), so
     # the user can still say make PREFIX=foo
     my($configure_prefix) = $Config{'prefix'};
-    $prefix = VMS::Filespec::unixify($prefix) if $Is_VMS;
+    $configure_prefix = VMS::Filespec::unixify($configure_prefix) if $Is_VMS;
     $self->{PREFIX} ||= $configure_prefix;
 
 

lib/ExtUtils/MakeMaker.pm
@@ -307,7 +307,7 @@ sub full_setup {
     @Get_from_Config = 
 	qw(
 	   ar cc cccdlflags ccdlflags dlext dlsrc ld lddlflags ldflags libc
-	   lib_ext obj_ext ranlib sitelibexp sitearchexp so
+	   lib_ext obj_ext ranlib sitelibexp sitearchexp so exe_ext
 	  );
 
     my $item;

perl11/cperl

commit 5a776c32ff44c1f9f78f065e45be6543356f7e38

Make dXSUB_SYS a declaration again
This was broken 1997 with 8cc95fdbe3687330bd987
We just add a PERL_UNUSED_VAR, which is only used on os2.
Move it after all declarations, it needs to be the last
(at least on os2).
Fixes GH #68

NetWare/interface.cpp
@@ -100,13 +100,12 @@ ClsPerlHost::PerlFree(PerlInterpreter *my_perl)
 ==============================================================================================*/
 
 static void xs_init(pTHX)
-//static void xs_init(pTHXo) //J
 {
 	const char *file = __FILE__;
 
-	dXSUB_SYS;
         dSP;
 	CV *cv = newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
+	dXSUB_SYS;
 	/* With cperl boot it now immediately */
         PUSHMARK(SP);
 	boot_DynaLoader(aTHX_ cv);

os2/os2ish.h
@@ -274,7 +274,7 @@ void Perl_OS2_term(void **excH, int exitstatus, int flags);
 /* #define PERL_SYS_TERM_BODY() STMT_START {	\
     if (Perl_HAB_set) WinTerminate(Perl_hab);	} STMT_END */
 
-#define dXSUB_SYS OS2_XS_init()
+#define dXSUB_SYS int fake = OS2_XS_init(); PERL_UNUSED_VAR(fake)
 
 #ifdef PERL_IS_AOUT
 /* #  define HAS_FORK */

os2/perlrexx.c
@@ -48,9 +48,9 @@ static void
 xs_init(pTHX)
 {
     const char *file = __FILE__;
-    dXSUB_SYS;
     dSP;
     CV *cv = newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
+    dXSUB_SYS;
     /* With cperl boot it now immediately */
     PUSHMARK(SP);
     boot_DynaLoader(aTHX_ cv);

symbian/PerlBase.cpp
@@ -175,9 +175,9 @@ EXTERN_C void boot_DynaLoader(pTHX_ CV* cv);
 static void xs_init(pTHX)
 {
     const char *file = __FILE__;
-    dXSUB_SYS;
     dSP;
     CV *cv = newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
+    dXSUB_SYS;
     /* With cperl boot it now immediately */
     PUSHMARK(SP);
     boot_DynaLoader(aTHX_ cv);

win32/perllib.c
@@ -35,9 +35,9 @@ static void
 xs_init(pTHX)
 {
     const char *file = __FILE__;
-    dXSUB_SYS;
     dSP;
     CV *cv = newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
+    dXSUB_SYS;
     /* With cperl boot it now immediately */
     PUSHMARK(SP);
     boot_DynaLoader(aTHX_ cv);