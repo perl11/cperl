cf10d0ca3075deb7bdf9880bbfaebc0e4f1fbca1
Tony Cook <tony@develop-help.com>
2015-10-28 14:11:12 +1100

    fix the non-Win32 build breakage introduced in ce9582af
    On Unix, EU::MM was falling back to EU::MM::version::vpp, which
    was removed above.
    
    Make the real version.pm available instead.

--- write_buildcustomize.pl
+++ write_buildcustomize.pl
@@ -39,6 +39,7 @@ my @toolchain = qw(cpan/AutoLoader/lib
                    ext/File-Find/lib
                    cpan/Text-Tabs/lib
 		   dist/constant/lib
+		   cpan/version/lib
 		   );
 
 # Text-ParseWords used only in ExtUtils::Liblist::Kid::_win32_ext()
