1febf97bf4449b64c8550085d687aee7cd5c6cb6
Tony Cook <tony@develop-help.com>
2015-10-29 14:13:36 +1100

    Unicode::Normalize needs CombiningClass.pl
    This (typically) worked ok for parallel builds, since U::N is built
    very late in the XS extensions build process, but for non-parallel
    builds the lack of the dependency could result in a build failure.
    
    # Conflicts:
    #	win32/makefile.mk

--- win32/makefile.mk
+++ win32/makefile.mk
@@ -1470,7 +1470,7 @@ $(PERLEXESTATIC): $(PERLSTATICLIB) $(CONFIGPM) $(PERLEXEST_OBJ) $(PERLEXE_RES)
 # DynaLoader.pm, so this will have to do
 
 #most of deps of this target are in DYNALOADER and therefore omitted here
-Extensions : $(PERLDEP) $(DYNALOADER) $(GLOBEXE) makeppport
+Extensions : $(PERLDEP) $(DYNALOADER) $(GLOBEXE) $(UNIDATAFILES) makeppport
 	$(MINIPERL) -I..\lib ..\make_ext.pl "MAKE=$(PLMAKE)" --dir=$(CPANDIR) --dir=$(DISTDIR) --dir=$(EXTDIR) --dynamic
 
 Extensions_reonly : $(PERLDEP) $(DYNALOADER) makeppport
