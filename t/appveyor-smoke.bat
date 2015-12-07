dade017bc15311336c2cad011f18aa133d52e2b5
Reini Urban <rurban@cpanel.net>
2015-12-07 12:13:28 +0100

    appveyor: smoke windows 32bit also, nmake only once
    nmake only once, otherwise XS deps are wrong, and will be rebuilt.
    nmake test is enough (thanks to bulk88, see
    1a0b1839951411d4f3dbdb2ff8d147cd878)

--- t/appveyor-smoke.bat
+++ t/appveyor-smoke.bat
@@ -4,14 +4,16 @@ if "%MSVC_VERSION%" == "10" goto msvc_10
 if "%MSVC_VERSION%" == "12" goto msvc_12
 
 :msvc_12
-call "C:\Program Files (x86)\Microsoft Visual Studio %MSVC_VERSION%.0\VC\vcvarsall.bat" amd64
+if "%PLATFORM%" == "x64" set PLATFORM=amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio %MSVC_VERSION%.0\VC\vcvarsall.bat" %PLATFORM%
+
 cd win32
-nmake CCTYPE=MSVC120 && nmake test TEST_JOBS=4 || exit /b
+nmake test CCTYPE=MSVC120 TEST_JOBS=4 || exit /b
 exit /b
 
 :msvc_10
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
-cd win32
-nmake CCTYPE=MSVC100 && nmake test TEST_JOBS=4 || exit /b
-exit /b
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /%PLATFORM%
 
+cd win32
+nmake test CCTYPE=MSVC100 TEST_JOBS=4 || exit /b
+exit /b
