ac787da31a01cc8f6a33a5186dff183768af6fe0
bulk88 <bulk88@hotmail.com>
2015-12-08 16:03:51 -0500

    appveyor: disable looking up env vars in registry for smoke speed
    Looking up env vars in the registry is a feature that was added by
    ActivePerl/ActiveState many years ago, some of their other products
    integrate themselves by adding PERL_* entries in the registry. These
    registry lookups (all registry lookups are eventually windows kernel
    syscalls), and the machine code implementing them in libperl, and the
    advapi32.dll dependency are not needed for smoking.

--- t/appveyor-smoke.bat
+++ t/appveyor-smoke.bat
@@ -9,12 +9,12 @@ if "%PLATFORM%" == "x64" set PLATFORM=amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio %MSVC_VERSION%.0\VC\vcvarsall.bat" %PLATFORM%
 
 cd win32
-nmake test CCTYPE=MSVC120 TEST_JOBS=4 || exit /b
+nmake test CCTYPE=MSVC120 USE_NO_REGISTRY=define TEST_JOBS=4 || exit /b
 exit /b
 
 :msvc_10
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /%PLATFORM%
 
 cd win32
-nmake test CCTYPE=MSVC100 TEST_JOBS=4 || exit /b
+nmake test CCTYPE=MSVC100 USE_NO_REGISTRY=define TEST_JOBS=4 || exit /b
 exit /b
