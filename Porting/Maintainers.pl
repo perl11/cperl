153486ad388086505d0be1f380a5900a0e14b80e
Reini Urban <rurban@cpanel.net>
2015-12-16 09:06:37 +0100

    Update to Time-Piece-1.31
    to fix MSVC smokes. See [cperl #90]

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1284,7 +1284,7 @@ use File::Glob qw(:case);
     },
 
     'Time::Piece' => {
-        'DISTRIBUTION' => 'RJBS/Time-Piece-1.30.tar.gz',
+        'DISTRIBUTION' => 'RJBS/Time-Piece-1.31.tar.gz',
         'FILES'        => q[cpan/Time-Piece],
     },
 
