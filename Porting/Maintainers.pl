c9117f77e191362f3c0f9e47ce8efc82061dea1b
bulk88 <bulk88@hotmail.com>
2016-01-02 17:26:10 -0500

    import Config 6.06
    6.06    Thu, Dec 31, 2015 11:18:29 PM
    -Fixed ActivePerl specific test fail
    -Fix Config_xs.in warning: request for implicit conversion from 'void *'
     to 'const char *' not permitted in C++ [-Wc++-compat] for strict C++
     compilation. (rurban)
    
    6.05_04 Tue, Dec 29, 2015  5:26:20 AM
    -Fixed CPAN RT#110631 "make install UNINST=1" broke the installed perl

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -222,7 +222,7 @@ use File::Glob qw(:case);
     },
 
     'Config' => {
-        'DISTRIBUTION' => 'BULKDD/other/XSConfig-6.05_03.tar.gz',
+        'DISTRIBUTION' => 'BULKDD/other/XSConfig-6.06.tar.gz',
         'FILES'      => q[
                  ext/Config/Config.pm
                  ext/Config/Config_xs.{in,out,PL}
