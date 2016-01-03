8135e8bb547ca538d25a1a303df314b881f73570
bulk88 <bulk88@hotmail.com>
2016-01-03 04:46:27 -0500

    Appveyor Win32 %PATH% cleanup/build speed
    Speedup appveyor the 4 smokes from 2:58hrs to 2:55, 1.7%.
    
    The default PATH on the appveyor box had 69 directories in it. Launching
    any process or loading any DLL is going to involve the Win32 PE loader
    searching PATH. nmake and dmake run each recipie line inside a unique
    cmd.exe process. Atleast on Server 2003, Win32 on Win64 (AKA WOW)
    Kernel32.dll tries to always load non-existent (never published by MS)
    debugging DLLs when kernel32.dll is loaded into the process. Since building
    and testing perl is launching around 10K-20K processes removing IO calls,
    even to a SSD, saves CPU and build time. Shorten the PATH to the minimum
    needed to build cperl. The following things have no business being in
    PATH when building cperl, things like go, php, python, ruby, system perl,
    misc package managers, performance instrumenting tools, .NET, powershell,
    hg, svn, SQL servers, and nodejs.
    
    Signed-off-by: Reini Urban <rurban@cpanel.net>

--- t/appveyor-smoke.bat
+++ t/appveyor-smoke.bat
@@ -7,6 +7,10 @@ if "%MSVC_VERSION%" == "12" goto msvc_12
 :msvc_12
 if "%PLATFORM%" == "x64" set PLATFORM=amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio %MSVC_VERSION%.0\VC\vcvarsall.bat" %PLATFORM%
+rem Env var PLATFORM changes, the X is really capitalized.
+rem Trim PATH to the minimum to build cperl less IO and CPU by not searching
+rem many dozens of dirs when loading DLLs and starting processes.
+if "%PLATFORM%" == "X64" ( set "PATH=C:\windows\system32;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\BIN\amd64;C:\Program Files (x86)\Windows Kits\8.1\bin\x64;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;C:\windows;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin" ) ELSE ( set "PATH=C:\windows\system32;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\BIN;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;C:\windows;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin" )
 
 cd win32
 nmake test CCTYPE=MSVC120 USE_NO_REGISTRY=define TEST_JOBS=4 || exit /b
@@ -14,6 +18,7 @@ exit /b
 
 :msvc_10
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /%PLATFORM%
+if "%PLATFORM%" == "x64" ( set "PATH=C:\windows\system32;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\Bin\amd64;C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\x64;C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin;C:\windows;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin" ) ELSE ( set "PATH=C:\windows\system32;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\Bin;C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE;C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin;C:\windows;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin" )
 
 cd win32
 nmake test CCTYPE=MSVC100 USE_NO_REGISTRY=define TEST_JOBS=4 || exit /b
