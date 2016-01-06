e392fcbc9172d780b7f98ac3de600cbad882173d
Karl Williamson <khw@cpan.org>
2016-01-06 13:00:37 -0700

    mktables: Fix /l testing in re/uniprops.t
    The utf8 locale testing was not getting done.
    
    # Conflicts:
    #	charclass_invlists.h
    #	regcharclass.h

--- regcharclass.h
+++ regcharclass.h
@@ -2514,7 +2514,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 2ed02f42659e99855dca83f91c652475b661d038f4ae552aceafa44fcde1f9e5 lib/unicore/mktables
+ * bdc75c5b4c672127502060e785c212eed39c4e92943ddcea7f9ea1f4b2ba3c38 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * 996abda3c0fbc2bfd575092af09e3b9b0331e624eb2e969a268457f8fd31ecbb regen/charset_translations.pl
  * 1c80f63db92ede11c1b80c61b7fb063ab6c63b7a8de43f2b3992487cd39a9323 regen/regcharclass.pl
