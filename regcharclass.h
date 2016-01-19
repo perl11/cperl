5579ddbe02d10b10d13f13bd774ccbc892292069
Karl Williamson <khw@cpan.org>
2016-01-18 23:01:07 -0700

    t/re/uniprops.t: Fix bug in diagnostic output
    An 'ord' was missing, so a warnings was raised.  This file is generated
    by mktables
    
    # Conflicts:
    #	charclass_invlists.h
    #	regcharclass.h

--- regcharclass.h
+++ regcharclass.h
@@ -1895,7 +1895,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * 68b4a90230f67c45ba3a0ed5fe6feb28fb3c51d8d705017e5f12ce7045ea70c9 lib/unicore/mktables
+ * 6688a8e89af0291d18c479916976e3723162eb2010e21c6dd611bad579e9e824 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * 913d2f93f3cb6cdf1664db888bf840bc4eb074eef824e082fceda24a9445e60c regen/charset_translations.pl
  * 1c80f63db92ede11c1b80c61b7fb063ab6c63b7a8de43f2b3992487cd39a9323 regen/regcharclass.pl
