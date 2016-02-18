5ac1f0a3e260c4472ddbc4e6d319d48460e8c6a7
Karl Williamson <khw@cpan.org>
2016-02-18 14:43:14 -0700

    Revamp -Dr handling of /[...]/
    This revamps the handling of -Dr for bracketed character classes.  There
    were bugs introduced earlier in 5.23, and this consolidates the handling
    of /d classes so that the interactions can be better considered.  It
    tries inverting the portion that is in the bitmap range to see if the
    output is shorter, and clearer that way.  And it always makes the
    above-bitmap code points show as not-inverted, as that is clearer.
    
    I ran out of time before the freeze, so I had to not invert in some
    cases.
    
    # Conflicts:
    #	proto.h

--- proto.h
+++ proto.h
@@ -5699,11 +5699,22 @@ STATIC const regnode*	S_dumpuntil(pTHX_ const regexp *r, const regnode *start, c
 #define PERL_ARGS_ASSERT_DUMPUNTIL	\
 	assert(r); assert(start); assert(node); assert(sv)
 
-STATIC bool	S_put_charclass_bitmap_innards(pTHX_ SV* sv, char* bitmap, SV** bitmap_invlist)
+STATIC bool	S_put_charclass_bitmap_innards(pTHX_ SV* sv, char* bitmap, SV* nonbitmap_invlist, SV* only_utf8_locale_invlist, const regnode * const node)
 			__attribute__nonnull__(pTHX_1)
 			__attribute__nonnull__(pTHX_2);
 #define PERL_ARGS_ASSERT_PUT_CHARCLASS_BITMAP_INNARDS	\
 	assert(sv); assert(bitmap)
+
+STATIC SV*	S_put_charclass_bitmap_innards_common(pTHX_ SV* invlist, SV* posixes, SV* only_utf8, SV* not_utf8, SV* only_utf8_locale, const bool invert)
+			__attribute__nonnull__(pTHX_1);
+#define PERL_ARGS_ASSERT_PUT_CHARCLASS_BITMAP_INNARDS_COMMON	\
+	assert(invlist)
+
+STATIC void	S_put_charclass_bitmap_innards_invlist(pTHX_ SV *sv, SV* invlist)
+			__attribute__nonnull__(pTHX_1)
+			__attribute__nonnull__(pTHX_2);
+#define PERL_ARGS_ASSERT_PUT_CHARCLASS_BITMAP_INNARDS_INVLIST	\
+	assert(sv); assert(invlist)
 
 STATIC void	S_put_code_point(pTHX_ SV* sv, UV c)
 			__attribute__nonnull__(pTHX_1);
@@ -7417,7 +7428,7 @@ PERL_CALLCONV SV*	Perl__new_invlist_C_array(pTHX_ const UV* const list)
 
 #endif
 #if defined(PERL_IN_REGCOMP_C) || defined(PERL_IN_REGEXEC_C)
-PERL_CALLCONV SV*	Perl__get_regclass_nonbitmap_data(pTHX_ const regexp *prog, const struct regnode *node, bool doinit, SV **listsvp, SV **lonly_utf8_locale, SV *exclude_list)
+PERL_CALLCONV SV*	Perl__get_regclass_nonbitmap_data(pTHX_ const regexp *prog, const struct regnode *node, bool doinit, SV **listsvp, SV **lonly_utf8_locale, SV **output_invlist)
 			__attribute__nonnull__(pTHX_2);
 #define PERL_ARGS_ASSERT__GET_REGCLASS_NONBITMAP_DATA	\
 	assert(node)
