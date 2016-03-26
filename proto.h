c25251f80ef69ddfdf470308d6dd1636c9c33c19
Daniel Dragan <bulk88@hotmail.com>
2016-03-26 13:44:30 -0400

    silence warnings in inline.h on Win64 VC build
    c:\p523\src\inline.h(211) : warning C4267: 'function' : conversion from 'size_t'
     to 'I32', possible loss of data
    c:\p523\src\inline.h(212) : warning C4267: 'function' : conversion from 'size_t'
     to 'I32', possible loss of data
    c:\p523\src\inline.h(421) : warning C4244: '=' : conversion from '__int64' to 'I
    32', possible loss of data
    c:\p523\src\inline.h(423) : warning C4244: '=' : conversion from '__int64' to 'I
    32', possible loss of data
    
    To fix the warnings at line 211 and 212, change the func to use a signed
    ptr length type. Although on x64, a 64b to 64b move instruction is 1 byte
    longer than a 32b to 32b move, so this commit adds a couple more bytes of
    machine code to the interp, but PVs len and cur are STRLEN, which is 64b
    on 64b OS, so something bad would happen if a very large off arg was
    passed to Perl_utf8_hop that was trucated to 32b, hence casting to silence
    the warning isn't appropriate, instead a bigger type is needed.
    
    S_cx_pushblock, a 8*(2^32), or 32 GB long perl stack malloc block is
    unrealistic. A 32 GB mark stack is infinite recursion. Cast away the
    warnings.

--- proto.h
+++ proto.h
@@ -5020,7 +5020,7 @@ PERL_CALLCONV IV	Perl_utf8_distance(pTHX_ const U8 *a, const U8 *b)
 #define PERL_ARGS_ASSERT_UTF8_DISTANCE	\
 	assert(a); assert(b)
 
-PERL_CALLCONV U8*	Perl_utf8_hop(const U8 *s, I32 off)
+PERL_CALLCONV U8*	Perl_utf8_hop(const U8 *s, SSize_t off)
 			__attribute__warn_unused_result__
 			__attribute__pure__
 			__attribute__nonnull__(1);
