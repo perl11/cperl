39cc247d4f576841378d87894464014e61e7ea57
Karl Williamson <khw@cpan.org>
2016-03-28 21:04:40 -0600

    Stop accepting deprecated NBSP in \N{}
    As scheduled for 5.26, this construct will no longer be accepted.

--- regcharclass.h
+++ regcharclass.h
@@ -9220,7 +9220,7 @@
  * 1a0687fb9c6c4567e853913549df0944fe40821279a3e9cdaa6ab8679bc286fd lib/unicore/extracted/DLineBreak.txt
  * 40bcfed3ca727c19e1331f6c33806231d5f7eeeabd2e6a9e06a3740c85d0c250 lib/unicore/extracted/DNumType.txt
  * a18d502bad39d527ac5586d7bc93e29f565859e3bcc24ada627eff606d6f5fed lib/unicore/extracted/DNumValues.txt
- * f3c212133b728cd6d1a14c833b36bb80758d3ce74239879b290f63169c6e1479 lib/unicore/mktables
+ * ba695b32928ff4d1918ef9bd4fc3d1478abe1fabb53246acfaf95a19ae7de290 lib/unicore/mktables
  * 462c9aaa608fb2014cd9649af1c5c009485c60b9c8b15b89401fdc10cf6161c6 lib/unicore/version
  * 913d2f93f3cb6cdf1664db888bf840bc4eb074eef824e082fceda24a9445e60c regen/charset_translations.pl
  * f74cf8f93b54c6a66158e351f8364f109d6dcfa5fb19e9058059af4b3daddb86 regen/regcharclass.pl
