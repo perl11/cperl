ef9f982d59620470034432705630d9ea1c5b9e3c
Karl Williamson <khw@cpan.org>
2016-05-10 17:34:25 -0600

    regen/embed.pl: Verify flags field of embed.fnc
    Make sure that the specified flags are legal.

--- regen/embed.pl
+++ regen/embed.pl
@@ -75,6 +75,9 @@ my ($embed, $core, $ext, $api) = setup_embed();
 	}
 
 	my ($flags,$retval,$plain_func,@args) = @$_;
+	if ($flags =~ / ( [^AabDdEfiMmnOoPpRrsUXx] ) /x) {
+	    warn "flag $1 is not legal (for function $plain_func)";
+	}
 	my (@nonnull, @unused, @names_of_nn, $func);
 	my $has_context = ( $flags !~ /n/ );
 	my $never_returns = ( $flags =~ /r/ );
