1bcef1352b6558c173b409af631ef43de245a2c9
Reini Urban <rurban@cpanel.net>
2016-06-08 13:15:37 +0200

    Bump Time::HiRes to 1.9733
    No changes from 5.22.3c

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1410,7 +1410,7 @@ use File::Glob qw(:case);
     },
 
     'Time::HiRes' => {
-        'DISTRIBUTION' => 'JHI/Time-HiRes-1.9732.tar.gz',
+        'DISTRIBUTION' => 'JHI/Time-HiRes-1.9733.tar.gz',
         'FILES'        => q[dist/Time-HiRes],
     },
 
