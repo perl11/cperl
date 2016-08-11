03bd02cca34162b3aa3673a4516eb716a7dd0d34
Reini Urban <rurban@cpanel.net>
2016-08-11 11:25:30 +0200

    Hash -DH: Harmonize keys
    use HvTOTALKEYS throughout for comparable keys/max numbers
    excluding any placeholders.
    It also helps with further possible cleared HVs.

--- hv.h
+++ hv.h
@@ -348,7 +348,7 @@ C<SV*>.
  */
 #define HvKEYS(hv)		HvUSEDKEYS(hv)
 #define HvUSEDKEYS(hv)		(HvTOTALKEYS(hv) - HvPLACEHOLDERS_get(hv))
-#define HvTOTALKEYS(hv)		XHvTOTALKEYS((XPVHV*)  SvANY(hv))
+#define HvTOTALKEYS(hv)		XHvTOTALKEYS((XPVHV*) SvANY(hv))
 #define HvPLACEHOLDERS(hv)	(*Perl_hv_placeholders_p(aTHX_ MUTABLE_HV(hv)))
 #define HvPLACEHOLDERS_get(hv)	(SvMAGIC(hv) ? Perl_hv_placeholders_get(aTHX_ (const HV *)hv) : 0)
 #define HvPLACEHOLDERS_set(hv,p)	Perl_hv_placeholders_set(aTHX_ MUTABLE_HV(hv), p)
