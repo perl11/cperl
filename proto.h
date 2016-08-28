880e5d6eccea3b5363acf2f426cd1818509aa4e9
Karl Williamson <khw@cpan.org>
2016-08-28 10:54:13 -0600

    Add is_utf8_valid_partial_char()
    This new function can test some purported UTF-8 to see if it is
    well-formed as far as it goes.  That is there aren't enough bytes for
    the character they start, but what is there is legal so far.  This can
    be useful in a fixed width buffer, where the final character is split in
    the middle, and we want to test without waiting for the next read that
    the entire buffer is valid.

--- proto.h
+++ proto.h
@@ -2703,6 +2703,13 @@ PERL_CALLCONV bool	Perl_is_utf8_upper(pTHX_ const U8 *p)
 #define PERL_ARGS_ASSERT_IS_UTF8_UPPER	\
 	assert(p)
 
+PERL_STATIC_INLINE bool	S_is_utf8_valid_partial_char(const U8 * const s, const U8 * const e)
+			__attribute__pure__
+			__attribute__nonnull__(1)
+			__attribute__nonnull__(2);
+#define PERL_ARGS_ASSERT_IS_UTF8_VALID_PARTIAL_CHAR	\
+	assert(s); assert(e)
+
 PERL_CALLCONV bool	Perl_is_utf8_xdigit(pTHX_ const U8 *p)
 			__attribute__global__
 			__attribute__deprecated__
