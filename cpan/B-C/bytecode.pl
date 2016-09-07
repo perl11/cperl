3b9cfd803418fd6f0ea92c5ed991b397f8bf34ed
Reini Urban <rurban@cpanel.net>
2016-09-07 18:07:31 +0200

    B-C: fix missing strNEc
    older perls dont have that

--- cpan/B-C/bytecode.pl
+++ cpan/B-C/bytecode.pl
@@ -145,6 +145,11 @@ print BYTERUN_C $c_header, <<'EOT';
   #define DEBUG_v(a) DEBUG_f(a)
 #endif
 
+#ifndef strNEc
+#  define strEQc(s, c) memEQ(s, ("" c ""), sizeof(c))
+#  define strNEc(s, c) memNE(s, ("" c ""), sizeof(c))
+#endif
+
 #include "byterun.h"
 #include "bytecode.h"
 
