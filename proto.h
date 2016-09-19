242db83489d4ae8d83ce6906ee135cdf32f7286a
Karl Williamson <khw@cpan.org>
2016-09-19 15:37:52 -0600

    utf8.c: Fix bug in new _is_utf8_char_helper() function
    This bug was exposed by the tests that I'm still developing

--- proto.h
+++ proto.h
@@ -6805,7 +6805,7 @@ STATIC SV *	S_incpush_if_exists(pTHX_ AV *const av, SV *dir, SV *const stem)
 #  endif
 #endif
 #if !defined(PERL_NO_INLINE_FUNCTIONS)
-PERL_CALLCONV STRLEN	Perl__is_utf8_char_helper(const U8 * const s, const U8 * const e, const U32 flags)
+PERL_CALLCONV STRLEN	Perl__is_utf8_char_helper(const U8 * const s, const U8 * e, const U32 flags)
 			__attribute__global__
 			__attribute__warn_unused_result__
 			__attribute__pure__
