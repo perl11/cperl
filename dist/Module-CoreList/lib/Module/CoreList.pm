2e98098c122ca202b65ef0bf4f158c49d529d34a
Reini Urban <rurban@cpanel.net>
2016-10-13 18:01:23 +0200

    Devel-PPPort: Bump to 3.35_01
    Nothing happened upstream

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -13310,6 +13310,7 @@ for my $version ( sort { version_sort($a, $b) } keys %released ) {
             'B'                     => '1.62_04',
             'Data::Dumper'          => '2.162',
             'Devel::Peek'           => '1.23_01',
+            'Devel::PPPort'         => '3.35_01',
             'File::Glob'            => '1.26_01',
             'File::DosGlob'         => '1.12_01',
             'POSIX'                 => '1.65_01',
