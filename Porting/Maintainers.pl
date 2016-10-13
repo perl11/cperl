61cf1588b8cb01d8afb44e66f374c81400ff2b92
Reini Urban <rurban@cpanel.net>
2016-10-13 14:55:43 +0200

    Archive-Tar: Update to 2.10

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -121,7 +121,7 @@ use File::Glob qw(:case);
 %Modules = (
 
     'Archive::Tar' => {
-        'DISTRIBUTION' => 'BINGOS/Archive-Tar-2.08.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/Archive-Tar-2.10.tar.gz',
         'FILES'        => q[cpan/Archive-Tar],
         'BUGS'         => 'bug-archive-tar@rt.cpan.org',
         'EXCLUDED'     => [
