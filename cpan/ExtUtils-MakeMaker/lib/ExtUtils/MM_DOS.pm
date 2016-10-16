48360bdbd4afece35ae91475f24d891172fac20f
Reini Urban <rurban@cpanel.net>
2016-10-16 20:11:45 +0200

    EUMM: bump MM*.pm to 8.04_04
    There is a test checking the versions

--- cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_DOS.pm
+++ cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_DOS.pm
@@ -2,7 +2,7 @@ package ExtUtils::MM_DOS;
 
 use strict;
 
-our $VERSION = '8.04_03';
+our $VERSION = '8.04_04';
 
 require ExtUtils::MM_Any;
 require ExtUtils::MM_Unix;
--- cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_MacOS.pm
+++ cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_MacOS.pm
@@ -2,7 +2,7 @@ package ExtUtils::MM_MacOS;
 
 use strict;
 
-our $VERSION = '8.04_03';
+our $VERSION = '8.04_04';
 
 sub new {
     die 'MacOS Classic (MacPerl) is no longer supported by MakeMaker';
