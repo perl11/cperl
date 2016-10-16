eef6b5fc323e1c9a3f604127a0afae88338a57d3
Reini Urban <rurban@cpanel.net>
2016-10-16 12:41:23 +0200

    Thread-Semaphore:  Update to 2.13
    Added down_timed method.

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -13365,6 +13365,7 @@ for my $version ( sort { version_sort($a, $b) } keys %released ) {
             'ExtUtils::Command::MM' => '8.04_04',
             'Storable'              => '3.01c',
             'Term::ReadKey'         => '2.37_01',
+            'Thread::Semaphore'     => '2.13',
             'Cpanel::JSON::XS'      => '3.0218',
             'CPAN::Meta'            => '2.150010c',
             'CPAN::Meta::Converter' => '2.150010',
