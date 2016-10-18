ff479302036dcc185d1af9cf8853da291404f5c1
Karl Williamson <khw@cpan.org>
2016-10-17 20:16:21 -0600

    op.c: Fix EBCDIC-only bug
    We have no tests that this fails for, but on an EBCDIC machine, the
    branches here are incorrect.  They are trying to determine if a UTF-8
    representation will be larger than a non-UTF-8 representation for code
    points < 256.  The proper test is if the code points are UTF-8
    invariant.

--- op.c
+++ op.c
@@ -5970,7 +5970,7 @@ S_pmtrans(pTHX_ OP *o, OP *expr, OP *repl)
 			tbl[i] = (short)i;
 		}
 		else {
-		    if (i < 128 && r[j] >= 128)
+		    if (UVCHR_IS_INVARIANT(i) && ! UVCHR_IS_INVARIANT(r[j]))
 			grows = 1;
 		    tbl[i] = r[j++];
 		}
@@ -6017,7 +6017,8 @@ S_pmtrans(pTHX_ OP *o, OP *expr, OP *repl)
 		--j;
 	    }
 	    if (tbl[t[i]] == -1) {
-		if (t[i] < 128 && r[j] >= 128)
+                if (     UVCHR_IS_INVARIANT(t[i])
+                    && ! UVCHR_IS_INVARIANT(r[j]))
 		    grows = 1;
 		tbl[t[i]] = r[j];
 	    }
