1f219ad28d9af46d3c6ce13bc7372d99590412c6
Reini Urban <rurban@cpanel.net>
2016-12-12 14:35:46 +0100

    podlators: Update to 4.09
    Add the t/data/snippets.
    
    Use Pod::Simple's logic to determine the native code
    points for NO BREAK SPACE and SOFT HYPHEN instead of hard-coding the
    ASCII values.  Hopefully fixes the case of mysterious disappearing
    open brackets on EBCDIC systems.  (#118240)

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1162,7 +1162,7 @@ use File::Glob qw(:case);
     },
 
     'Pod::Man' => {
-        'DISTRIBUTION' => 'RRA/podlators-4.08.tar.gz',
+        'DISTRIBUTION' => 'RRA/podlators-4.09.tar.gz',
         'FILES'        => q[cpan/podlators pod/perlpodstyle.pod],
 
         # cperl fix to support make -s
@@ -1178,6 +1178,7 @@ use File::Glob qw(:case);
         },
         'EXCLUDED' => [
           qr{^t/(style|docs)/},
+          qr{/docs/},
         ],
     },
 
