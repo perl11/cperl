11045611d2442542ceeff48a403429afb1466357
Reini Urban <rurban@cpanel.net>
2016-12-14 14:30:42 +0100

    podlators  4.09
    Many Pod::Man bugfixes and new tests.
    See https://metacpan.org/changes/distribution/podlators

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1165,7 +1165,7 @@ use File::Glob qw(:case);
     },
 
     'Pod::Man' => {
-        'DISTRIBUTION' => 'RRA/podlators-4.08.tar.gz',
+        'DISTRIBUTION' => 'RRA/podlators-4.09.tar.gz',
         'FILES'        => q[cpan/podlators pod/perlpodstyle.pod],
 
         # cperl fix to support make -s
@@ -1181,6 +1181,7 @@ use File::Glob qw(:case);
         },
         'EXCLUDED' => [
           qr{^t/(style|docs)/},
+          qr{/docs/},
         ],
     },
 
