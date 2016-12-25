Perl/perl5

Exempt @DB::args from ‘used once’ warnings
A perfectly benign use of @DB::args like

sub foo { package DB { () = caller 0 } print "@DB::args\n" }
foo(1..3);

warns under -w, though that is exactly its intended use.  That is
is not nice.

warnings.pm itself uses @DB::args, which is why this went unnoticed
for so many years.

This commit eliminates the ‘used once’ warning, but the ‘Possible
unintended interpolation’ message that that code produces will be
handled in another commit.

gv.c
@@ -1831,6 +1831,10 @@ S_gv_magicalize(pTHX_ GV *gv, HV *stash, const char *name, STRLEN len,
 		    GvMULTI_on(gv);
 		break;
 	    case 'a':
+		if (stash == PL_debstash && len==4 && strEQ(name2,"rgs")) {
+		    GvMULTI_on(gv_AVadd(gv));
+		    break;
+		}
 	    case 'b':
 		if (len == 1 && sv_type == SVt_PV)
 		    GvMULTI_on(gv);

perl11/cperl

gv.c
@@ -1856,6 +1856,11 @@ S_gv_magicalize(pTHX_ GV *gv, HV *stash, const char *name, STRLEN len,
 		    GvMULTI_on(gv);
 		break;
 	    case 'a':
+		if (stash == PL_debstash && len==4 && memEQc(name,"args")) {
+		    GvMULTI_on(gv_AVadd(gv));
+		    break;
+		}
+		/* FALLTHROUGH for a */
 	    case 'b':
 		if (len == 1 && sv_type == SVt_PV)
 		    GvMULTI_on(gv);