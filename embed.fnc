Perl/perl5

Add new synonym 'is_utf8_invariant_string'
This is clearer as to its meaning than the existing 'is_ascii_string'
and 'is_invariant_string', which are retained for back compat.  The
thread context variable is removed as it is not used.

embed.fnc
@@ -731,8 +731,9 @@ ADMpPR	|bool	|is_uni_lower_lc|UV c
 ADMpPR	|bool	|is_uni_print_lc|UV c
 ADMpPR	|bool	|is_uni_punct_lc|UV c
 ADMpPR	|bool	|is_uni_xdigit_lc|UV c
-AnpdR	|bool	|is_invariant_string|NN const U8 *s|STRLEN len
-AmpdR	|bool	|is_ascii_string|NN const U8 *s|STRLEN len
+AnpdR	|bool	|is_utf8_invariant_string|NN const U8 *s|STRLEN len
+AmnpdR	|bool	|is_ascii_string|NN const U8 *s|STRLEN len
+AmnpdR	|bool	|is_invariant_string|NN const U8 *s|STRLEN len
 AnpdD	|STRLEN	|is_utf8_char	|NN const U8 *s
 Abmnpd	|STRLEN	|is_utf8_char_buf|NN const U8 *buf|NN const U8 *buf_end
 Anpd	|bool	|is_utf8_string	|NN const U8 *s|STRLEN len

perl11/cperl

embed.fnc
@@ -740,8 +740,9 @@ ADMpPR	|bool	|is_uni_lower_lc	|UV c
 ADMpPR	|bool	|is_uni_print_lc	|UV c
 ADMpPR	|bool	|is_uni_punct_lc	|UV c
 ADMpPR	|bool	|is_uni_xdigit_lc	|UV c
-AnpdR	|bool	|is_invariant_string	|NN const U8 *s|STRLEN len
-AmpdR	|bool	|is_ascii_string	|NN const U8 *s|STRLEN len
+AnpdR	|bool	|is_utf8_invariant_string|NN const U8 *s|STRLEN len
+AmnpdR	|bool	|is_ascii_string	|NN const U8 *s|STRLEN len
+AmnpdR	|bool	|is_invariant_string	|NN const U8 *s|STRLEN len
 AnpdD	|STRLEN	|is_utf8_char		|NN const U8 *s
 Abmnpd	|STRLEN	|is_utf8_char_buf	|NN const U8 *buf|NN const U8 *buf_end
 Anpd	|bool	|is_utf8_string		|NN const U8 *s|STRLEN len