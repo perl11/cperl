Perl/perl5

Add branch predictors for DEBUG statements
It is unlikely that any given debug flag will be set in any given run,
even under DEBUGGING builds.

perl.h
@@ -4192,34 +4192,34 @@ Gid_t getegid (void);
 #define DEBUG_DB_RECURSE_FLAG	0x40000000
 #define DEBUG_TOP_FLAG		0x80000000 /* -D was given --> PL_debug |= FLAG */
 
-#  define DEBUG_p_TEST_ (PL_debug & DEBUG_p_FLAG)
-#  define DEBUG_s_TEST_ (PL_debug & DEBUG_s_FLAG)
-#  define DEBUG_l_TEST_ (PL_debug & DEBUG_l_FLAG)
-#  define DEBUG_t_TEST_ (PL_debug & DEBUG_t_FLAG)
-#  define DEBUG_o_TEST_ (PL_debug & DEBUG_o_FLAG)
-#  define DEBUG_c_TEST_ (PL_debug & DEBUG_c_FLAG)
-#  define DEBUG_P_TEST_ (PL_debug & DEBUG_P_FLAG)
-#  define DEBUG_m_TEST_ (PL_debug & DEBUG_m_FLAG)
-#  define DEBUG_f_TEST_ (PL_debug & DEBUG_f_FLAG)
-#  define DEBUG_r_TEST_ (PL_debug & DEBUG_r_FLAG)
-#  define DEBUG_x_TEST_ (PL_debug & DEBUG_x_FLAG)
-#  define DEBUG_u_TEST_ (PL_debug & DEBUG_u_FLAG)
-#  define DEBUG_U_TEST_ (PL_debug & DEBUG_U_FLAG)
-#  define DEBUG_H_TEST_ (PL_debug & DEBUG_H_FLAG)
-#  define DEBUG_X_TEST_ (PL_debug & DEBUG_X_FLAG)
-#  define DEBUG_D_TEST_ (PL_debug & DEBUG_D_FLAG)
-#  define DEBUG_S_TEST_ (PL_debug & DEBUG_S_FLAG)
-#  define DEBUG_T_TEST_ (PL_debug & DEBUG_T_FLAG)
-#  define DEBUG_R_TEST_ (PL_debug & DEBUG_R_FLAG)
-#  define DEBUG_J_TEST_ (PL_debug & DEBUG_J_FLAG)
-#  define DEBUG_v_TEST_ (PL_debug & DEBUG_v_FLAG)
-#  define DEBUG_C_TEST_ (PL_debug & DEBUG_C_FLAG)
-#  define DEBUG_A_TEST_ (PL_debug & DEBUG_A_FLAG)
-#  define DEBUG_q_TEST_ (PL_debug & DEBUG_q_FLAG)
-#  define DEBUG_M_TEST_ (PL_debug & DEBUG_M_FLAG)
-#  define DEBUG_B_TEST_ (PL_debug & DEBUG_B_FLAG)
-#  define DEBUG_L_TEST_ (PL_debug & DEBUG_L_FLAG)
-#  define DEBUG_i_TEST_ (PL_debug & DEBUG_i_FLAG)
+#  define DEBUG_p_TEST_ UNLIKELY(PL_debug & DEBUG_p_FLAG)
+#  define DEBUG_s_TEST_ UNLIKELY(PL_debug & DEBUG_s_FLAG)
+#  define DEBUG_l_TEST_ UNLIKELY(PL_debug & DEBUG_l_FLAG)
+#  define DEBUG_t_TEST_ UNLIKELY(PL_debug & DEBUG_t_FLAG)
+#  define DEBUG_o_TEST_ UNLIKELY(PL_debug & DEBUG_o_FLAG)
+#  define DEBUG_c_TEST_ UNLIKELY(PL_debug & DEBUG_c_FLAG)
+#  define DEBUG_P_TEST_ UNLIKELY(PL_debug & DEBUG_P_FLAG)
+#  define DEBUG_m_TEST_ UNLIKELY(PL_debug & DEBUG_m_FLAG)
+#  define DEBUG_f_TEST_ UNLIKELY(PL_debug & DEBUG_f_FLAG)
+#  define DEBUG_r_TEST_ UNLIKELY(PL_debug & DEBUG_r_FLAG)
+#  define DEBUG_x_TEST_ UNLIKELY(PL_debug & DEBUG_x_FLAG)
+#  define DEBUG_u_TEST_ UNLIKELY(PL_debug & DEBUG_u_FLAG)
+#  define DEBUG_U_TEST_ UNLIKELY(PL_debug & DEBUG_U_FLAG)
+#  define DEBUG_H_TEST_ UNLIKELY(PL_debug & DEBUG_H_FLAG)
+#  define DEBUG_X_TEST_ UNLIKELY(PL_debug & DEBUG_X_FLAG)
+#  define DEBUG_D_TEST_ UNLIKELY(PL_debug & DEBUG_D_FLAG)
+#  define DEBUG_S_TEST_ UNLIKELY(PL_debug & DEBUG_S_FLAG)
+#  define DEBUG_T_TEST_ UNLIKELY(PL_debug & DEBUG_T_FLAG)
+#  define DEBUG_R_TEST_ UNLIKELY(PL_debug & DEBUG_R_FLAG)
+#  define DEBUG_J_TEST_ UNLIKELY(PL_debug & DEBUG_J_FLAG)
+#  define DEBUG_v_TEST_ UNLIKELY(PL_debug & DEBUG_v_FLAG)
+#  define DEBUG_C_TEST_ UNLIKELY(PL_debug & DEBUG_C_FLAG)
+#  define DEBUG_A_TEST_ UNLIKELY(PL_debug & DEBUG_A_FLAG)
+#  define DEBUG_q_TEST_ UNLIKELY(PL_debug & DEBUG_q_FLAG)
+#  define DEBUG_M_TEST_ UNLIKELY(PL_debug & DEBUG_M_FLAG)
+#  define DEBUG_B_TEST_ UNLIKELY(PL_debug & DEBUG_B_FLAG)
+#  define DEBUG_L_TEST_ UNLIKELY(PL_debug & DEBUG_L_FLAG)
+#  define DEBUG_i_TEST_ UNLIKELY(PL_debug & DEBUG_i_FLAG)
 #  define DEBUG_Xv_TEST_ (DEBUG_X_TEST_ && DEBUG_v_TEST_)
 #  define DEBUG_Uv_TEST_ (DEBUG_U_TEST_ && DEBUG_v_TEST_)
 #  define DEBUG_Pv_TEST_ (DEBUG_P_TEST_ && DEBUG_v_TEST_)

perl11/cperl

perl.h
@@ -4300,43 +4300,43 @@ Gid_t getegid (void);
 #define DEBUG_DB_RECURSE_FLAG	0x40000000
 #define DEBUG_TOP_FLAG		0x80000000 /* -D was given --> PL_debug |= FLAG */
 
-#  define DEBUG_p_TEST_ (PL_debug & DEBUG_p_FLAG)
-#  define DEBUG_s_TEST_ (PL_debug & DEBUG_s_FLAG)
-#  define DEBUG_l_TEST_ (PL_debug & DEBUG_l_FLAG)
-#  define DEBUG_t_TEST_ (PL_debug & DEBUG_t_FLAG)
-#  define DEBUG_o_TEST_ (PL_debug & DEBUG_o_FLAG)
-#  define DEBUG_c_TEST_ (PL_debug & DEBUG_c_FLAG)
-#  define DEBUG_P_TEST_ (PL_debug & DEBUG_P_FLAG)
-#  define DEBUG_m_TEST_ (PL_debug & DEBUG_m_FLAG)
-#  define DEBUG_f_TEST_ (PL_debug & DEBUG_f_FLAG)
-#  define DEBUG_r_TEST_ (PL_debug & DEBUG_r_FLAG)
-#  define DEBUG_x_TEST_ (PL_debug & DEBUG_x_FLAG)
-#  define DEBUG_u_TEST_ (PL_debug & DEBUG_u_FLAG)
-#  define DEBUG_U_TEST_ (PL_debug & DEBUG_U_FLAG)
-#  define DEBUG_H_TEST_ (PL_debug & DEBUG_H_FLAG)
-#  define DEBUG_X_TEST_ (PL_debug & DEBUG_X_FLAG)
-#  define DEBUG_D_TEST_ (PL_debug & DEBUG_D_FLAG)
-#  define DEBUG_S_TEST_ (PL_debug & DEBUG_S_FLAG)
-#  define DEBUG_T_TEST_ (PL_debug & DEBUG_T_FLAG)
-#  define DEBUG_R_TEST_ (PL_debug & DEBUG_R_FLAG)
-#  define DEBUG_J_TEST_ (PL_debug & DEBUG_J_FLAG)
-#  define DEBUG_v_TEST_ (PL_debug & DEBUG_v_FLAG)
-#  define DEBUG_C_TEST_ (PL_debug & DEBUG_C_FLAG)
-#  define DEBUG_A_TEST_ (PL_debug & DEBUG_A_FLAG)
-#  define DEBUG_q_TEST_ (PL_debug & DEBUG_q_FLAG)
-#  define DEBUG_M_TEST_ (PL_debug & DEBUG_M_FLAG)
-#  define DEBUG_B_TEST_ (PL_debug & DEBUG_B_FLAG)
-#  define DEBUG_L_TEST_ (PL_debug & DEBUG_L_FLAG)
-#  define DEBUG_I_TEST_ (PL_debug & DEBUG_I_FLAG)
-#  define DEBUG_k_TEST_ (PL_debug & DEBUG_k_FLAG)
-#  define DEBUG_kv_TEST_ (DEBUG_k_TEST_ && DEBUG_v_TEST_)
-#  define DEBUG_Xv_TEST_ (DEBUG_X_TEST_ && DEBUG_v_TEST_)
-#  define DEBUG_Uv_TEST_ (DEBUG_U_TEST_ && DEBUG_v_TEST_)
-#  define DEBUG_Pv_TEST_ (DEBUG_P_TEST_ && DEBUG_v_TEST_)
-#  define DEBUG_Iv_TEST_ (DEBUG_I_TEST_ && DEBUG_v_TEST_)
-#  define DEBUG_Lv_TEST_ (DEBUG_L_TEST_ && DEBUG_v_TEST_)
-#  define DEBUG_lv_TEST_ (DEBUG_l_TEST_ && DEBUG_v_TEST_)
-#  define DEBUG_mv_TEST_ (DEBUG_m_TEST_ && DEBUG_v_TEST_)
+#  define DEBUG_p_TEST_  UNLIKELY(PL_debug & DEBUG_p_FLAG)
+#  define DEBUG_s_TEST_  UNLIKELY(PL_debug & DEBUG_s_FLAG)
+#  define DEBUG_l_TEST_  UNLIKELY(PL_debug & DEBUG_l_FLAG)
+#  define DEBUG_t_TEST_  UNLIKELY(PL_debug & DEBUG_t_FLAG)
+#  define DEBUG_o_TEST_  UNLIKELY(PL_debug & DEBUG_o_FLAG)
+#  define DEBUG_c_TEST_  UNLIKELY(PL_debug & DEBUG_c_FLAG)
+#  define DEBUG_P_TEST_  UNLIKELY(PL_debug & DEBUG_P_FLAG)
+#  define DEBUG_m_TEST_  UNLIKELY(PL_debug & DEBUG_m_FLAG)
+#  define DEBUG_f_TEST_  UNLIKELY(PL_debug & DEBUG_f_FLAG)
+#  define DEBUG_r_TEST_  UNLIKELY(PL_debug & DEBUG_r_FLAG)
+#  define DEBUG_x_TEST_  UNLIKELY(PL_debug & DEBUG_x_FLAG)
+#  define DEBUG_u_TEST_  UNLIKELY(PL_debug & DEBUG_u_FLAG)
+#  define DEBUG_U_TEST_  UNLIKELY(PL_debug & DEBUG_U_FLAG)
+#  define DEBUG_H_TEST_  UNLIKELY(PL_debug & DEBUG_H_FLAG)
+#  define DEBUG_X_TEST_  UNLIKELY(PL_debug & DEBUG_X_FLAG)
+#  define DEBUG_D_TEST_  UNLIKELY(PL_debug & DEBUG_D_FLAG)
+#  define DEBUG_S_TEST_  UNLIKELY(PL_debug & DEBUG_S_FLAG)
+#  define DEBUG_T_TEST_  UNLIKELY(PL_debug & DEBUG_T_FLAG)
+#  define DEBUG_R_TEST_  UNLIKELY(PL_debug & DEBUG_R_FLAG)
+#  define DEBUG_J_TEST_  UNLIKELY(PL_debug & DEBUG_J_FLAG)
+#  define DEBUG_v_TEST_  UNLIKELY(PL_debug & DEBUG_v_FLAG)
+#  define DEBUG_C_TEST_  UNLIKELY(PL_debug & DEBUG_C_FLAG)
+#  define DEBUG_A_TEST_  UNLIKELY(PL_debug & DEBUG_A_FLAG)
+#  define DEBUG_q_TEST_  UNLIKELY(PL_debug & DEBUG_q_FLAG)
+#  define DEBUG_M_TEST_  UNLIKELY(PL_debug & DEBUG_M_FLAG)
+#  define DEBUG_B_TEST_  UNLIKELY(PL_debug & DEBUG_B_FLAG)
+#  define DEBUG_L_TEST_  UNLIKELY(PL_debug & DEBUG_L_FLAG)
+#  define DEBUG_I_TEST_  UNLIKELY(PL_debug & DEBUG_I_FLAG)
+#  define DEBUG_k_TEST_  UNLIKELY(PL_debug & DEBUG_k_FLAG)
+#  define DEBUG_kv_TEST_ UNLIKELY((PL_debug & DEBUG_k_FLAG|DEBUG_v_FLAG) == (DEBUG_k_FLAG|DEBUG_v_FLAG))
+#  define DEBUG_Xv_TEST_ UNLIKELY((PL_debug & DEBUG_X_FLAG|DEBUG_v_FLAG) == (DEBUG_X_FLAG|DEBUG_v_FLAG))
+#  define DEBUG_Uv_TEST_ UNLIKELY((PL_debug & DEBUG_U_FLAG|DEBUG_v_FLAG) == (DEBUG_U_FLAG|DEBUG_v_FLAG))
+#  define DEBUG_Pv_TEST_ UNLIKELY((PL_debug & DEBUG_P_FLAG|DEBUG_v_FLAG) == (DEBUG_P_FLAG|DEBUG_v_FLAG))
+#  define DEBUG_Iv_TEST_ UNLIKELY((PL_debug & DEBUG_I_FLAG|DEBUG_v_FLAG) == (DEBUG_I_FLAG|DEBUG_v_FLAG))
+#  define DEBUG_Lv_TEST_ UNLIKELY((PL_debug & DEBUG_L_FLAG|DEBUG_v_FLAG) == (DEBUG_L_FLAG|DEBUG_v_FLAG))
+#  define DEBUG_lv_TEST_ UNLIKELY((PL_debug & DEBUG_l_FLAG|DEBUG_v_FLAG) == (DEBUG_l_FLAG|DEBUG_v_FLAG))
+#  define DEBUG_mv_TEST_ UNLIKELY((PL_debug & DEBUG_m_FLAG|DEBUG_v_FLAG) == (DEBUG_m_FLAG|DEBUG_v_FLAG))
 
 #ifdef DEBUGGING
 