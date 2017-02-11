1b77c24015edff4562e678a55406b224b57e915a
Reini Urban <rurban@cpan.org>
2017-02-11 23:24:36 +0100

    Compress-Raw-Zlib: -Wc++compat fixes
    and build our own zlib, as the system libzlib
    seems to be outdated.

--- cpan/Compress-Raw-Zlib/zlib-src/inflate.c
+++ cpan/Compress-Raw-Zlib/zlib-src/inflate.c
@@ -1487,8 +1487,8 @@ z_streamp source;
     }
 
     /* copy state */
-    zmemcpy((voidpf)dest, (voidpf)source, sizeof(z_stream));
-    zmemcpy((voidpf)copy, (voidpf)state, sizeof(struct inflate_state));
+    zmemcpy((Bytef*)dest, (Bytef*)source, sizeof(z_stream));
+    zmemcpy((Bytef*)copy, (Bytef*)state, sizeof(struct inflate_state));
     copy->strm = dest;
     if (state->lencode >= state->codes &&
         state->lencode <= state->codes + ENOUGH - 1) {
