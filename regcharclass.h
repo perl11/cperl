634fa48ea95866d1be59d094f6fad4962f4325a0
Karl Williamson <khw@cpan.org>
2017-02-19 22:03:27 -0700

    Balance uniprops tests
    Commit 5656b1f654bb034c561558968ed3cf87a737b3e1 split the tests
    generated by mktables so that 10 separate files each execute 10% of the
    tests.  But it turns out that some tests are much more involved than
    others, so that some of those 10 files still took much longer than
    average.  This commit changes the split so that the amount of time each
    file takes is more balanced.  It uses a natural breaking spot for the
    tests for the \b{} flavors, except that GCB and SB are each short (so
    are combined into being tested from one file), and LB is very long, so
    is split into 4 test groups.

--- regcharclass.h
+++ regcharclass.h
@@ -8364,7 +8364,7 @@
  * 37f6186253da9824bdb27f4ad867bfe8c25d4dc6bdb2f05585e40a034675a348 lib/unicore/extracted/DLineBreak.txt
  * ef24061b5a5dc93d7e90c2e34530ec757180ee75d872cba65ffc946e52624ae8 lib/unicore/extracted/DNumType.txt
  * a197371fec9a1b517058b440841f60f9378d81682084eef8db22a88cb2f96e90 lib/unicore/extracted/DNumValues.txt
- * 3c670fafd7ebe0e28bc2dd39e0ac0e1508a80e220cff19dd2f5f7184b1043cc3 lib/unicore/mktables
+ * 6757ed0d9631d00c05560b9c66703b7ba9764ab05512066e0feaaeba329c1a07 lib/unicore/mktables
  * cdecb300baad839a6f62791229f551a4fa33f3cbdca08e378dc976466354e778 lib/unicore/version
  * 913d2f93f3cb6cdf1664db888bf840bc4eb074eef824e082fceda24a9445e60c regen/charset_translations.pl
  * 916174aa4a5e65e5686d2bda8522d335603b9c92bc0ec4c1eaa5d3c6ac958522 regen/regcharclass.pl
