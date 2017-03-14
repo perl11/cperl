c1491eebfcc90762bbcdb62b3878bc39b1abcc45
David Mitchell <davem@iabyn.com>
2017-03-14 09:19:15 +0000

    S_require_tie_mod(): use a new stack
    RT #130861
    
    This function is used to load a module associated with various magic vars,
    like $[ and %+. Since it can be called 'unexpectedly', it should use a new
    stack. The issue in this ticket was equivalent to
    
        my $var = '[';
        $$var;
    
    where the symbolic dereference triggered a run-time load of arybase.pm,
    which grew the stack, invalidating the SP in pp_rv2sv.
    
    Note that most of the stuff which S_require_tie_mod() calls, such as
    load_module(), will do its own PUSHSTACK(); but S_require_tie_mod() also
    does a bit of stack manipulation itself.
    
    The test case includes a magic number, 125, which happens to be the exact
    size necessary to trigger a stack realloc in S_require_tie_mod(). In later
    perl versions this value may well change. But it seemed too expensive
    to call fresh_perl_is() 100's of times with different values of $n.
    
    This commit also adds a SPAGAIN to pp_rv2sv on the 'belt and braces'
    principle.
    
    This commit is based on an earlier effort by Aaron Crane.

--- gv.c
+++ gv.c
@@ -1357,6 +1357,7 @@ S_require_tie_mod(pTHX_ GV *gv, const char varname, const char * name,
         GV **gvp;
         dSP;
 
+        PUSHSTACKi(PERLSI_MAGIC);
         ENTER;
 
 #define HV_FETCH_TIE_FUNC (GV **)hv_fetchs_ifexists(stash, "_tie_it", 0)
@@ -1386,6 +1387,7 @@ S_require_tie_mod(pTHX_ GV *gv, const char varname, const char * name,
         PUTBACK;
         call_sv((SV *)*gvp, G_VOID|G_DISCARD);
         LEAVE;
+        POPSTACK;
     }
 }
 
