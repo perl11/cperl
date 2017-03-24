80dfd710ced6149d1f8dea6e2a1a2dc531a1b104
Karl Williamson <khw@cpan.org>
2017-03-23 19:38:04 -0600

    handy.h: Make uniform the defns of strFOO, memFOO
    For consistency

--- handy.h
+++ handy.h
@@ -477,15 +477,15 @@ Returns zero if non-equal, or non-zero if equal.
 */
 
 
-#define strNE(s1,s2) (strcmp(s1,s2))
-#define strEQ(s1,s2) (!strcmp(s1,s2))
+#define strNE(s1,s2) (strcmp(s1,s2) != 0)
+#define strEQ(s1,s2) (strcmp(s1,s2) == 0)
 #define strLT(s1,s2) (strcmp(s1,s2) < 0)
 #define strLE(s1,s2) (strcmp(s1,s2) <= 0)
 #define strGT(s1,s2) (strcmp(s1,s2) > 0)
 #define strGE(s1,s2) (strcmp(s1,s2) >= 0)
 
-#define strnNE(s1,s2,l) (strncmp(s1,s2,l))
-#define strnEQ(s1,s2,l) (!strncmp(s1,s2,l))
+#define strnNE(s1,s2,l) (strncmp(s1,s2,l) != 0)
+#define strnEQ(s1,s2,l) (strncmp(s1,s2,l) == 0)
 
 /* These names are controversial, so guarding against their being used in more
  * places than they already are. Previously called strEQs */
@@ -496,8 +496,8 @@ Returns zero if non-equal, or non-zero if equal.
 #define strBEGINs(s1,s2) (strncmp(s1,"" s2 "", sizeof(s2)-1) == 0)
 #endif
 
-#define memNE(s1,s2,l) (memcmp(s1,s2,l))
-#define memEQ(s1,s2,l) (!memcmp(s1,s2,l))
+#define memNE(s1,s2,l) (memcmp(s1,s2,l) != 0)
+#define memEQ(s1,s2,l) (memcmp(s1,s2,l) == 0)
 
 /* memEQ and memNE where second comparand is a string constant. But rather use memEQc */
 #define memEQs(s1, l, s2) \
