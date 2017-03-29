9ab519180101ee9a6f48fce85df24440a5237aa6
Reini Urban <rurban@cpan.org>
2017-03-29 14:35:29 +0200

    perlio: improve PerlSIO_set_ptr with C++

--- perlio.c
+++ perlio.c
@@ -3638,7 +3638,8 @@ PerlIOStdio_set_ptrcnt(pTHX_ PerlIO *f, STDCHAR * ptr, SSize_t cnt)
          *
          * So let's try silencing the warning at least for gcc. */
 #ifdef __cplusplus
-	PerlSIO_set_ptr(stdio, (U8*)ptr);
+        U8* uptr = (U8*)ptr;
+	PerlSIO_set_ptr(stdio, uptr);
 #else
         GCC_DIAG_IGNORE(-Wpointer-sign);
 	PerlSIO_set_ptr(stdio, ptr); /* LHS STDCHAR* cast non-portable */
