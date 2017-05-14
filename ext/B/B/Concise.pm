c3a343b3073e7a882a4beb1448f16a2dc00b1e55
Reini Urban <rurban@cpan.org>
2017-05-14 10:54:35 +0200

    B-Concise 0.9991c: bump version

--- ext/B/B/Concise.pm
+++ ext/B/B/Concise.pm
@@ -14,7 +14,7 @@ use warnings; # uses #3 and #4, since warnings uses Carp
 
 use Exporter (); # use #5
 
-our $VERSION   = "0.999c";
+our $VERSION   = "0.9991c";
 $VERSION =~ s/c$//;
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw( set_style set_style_standard add_callback
