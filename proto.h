d17359e05d3e7653c67ef9b2f8482f93612a4e70
Karl Williamson <khw@cpan.org>
2017-06-09 11:44:58 -0600

    bytes_from_utf8(): parameter must not be NULL
    The function assumes that the parameter is not NULL.  Declare that to
    embed.fnc.  Also change the name to indicate that it is a pointer.

--- proto.h
+++ proto.h
@@ -473,12 +473,13 @@ PERL_CALLCONV int	Perl_bytes_cmp_utf8(pTHX_ const U8 *b, STRLEN blen, const U8 *
 #define PERL_ARGS_ASSERT_BYTES_CMP_UTF8	\
 	assert(b); assert(u)
 
-PERL_CALLCONV U8*	Perl_bytes_from_utf8(pTHX_ const U8 *s, STRLEN *lenp, bool *is_utf8)
+PERL_CALLCONV U8*	Perl_bytes_from_utf8(pTHX_ const U8 *s, STRLEN *lenp, bool *is_utf8p)
 			__attribute__global__
 			__attribute__nonnull__(pTHX_1)
-			__attribute__nonnull__(pTHX_2);
+			__attribute__nonnull__(pTHX_2)
+			__attribute__nonnull__(pTHX_3);
 #define PERL_ARGS_ASSERT_BYTES_FROM_UTF8	\
-	assert(s); assert(lenp)
+	assert(s); assert(lenp); assert(is_utf8p)
 
 PERL_CALLCONV U8*	Perl_bytes_to_utf8(pTHX_ const U8 *s, STRLEN *lenp)
 			__attribute__global__
