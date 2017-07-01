19ad3c4c94156cf09b53e64cf8594d8903c6847c
Karl Williamson <khw@cpan.org>
2017-07-01 07:21:09 -0600

    utf8.c: Generalize static fcn return for indeterminate result
    This makes it harder to think that 0 means a definite FALSE.

--- proto.h
+++ proto.h
@@ -10570,7 +10570,7 @@ STATIC UV	S_check_locale_boundary_crossing(pTHX_ const U8* const p, const UV res
 	assert(p); assert(ustrp); assert(lenp)
 
 #ifndef PERL_NO_INLINE_FUNCTIONS
-PERL_STATIC_INLINE bool	S_does_utf8_overflow(const U8 * const s, const U8 * e)
+PERL_STATIC_INLINE int	S_does_utf8_overflow(const U8 * const s, const U8 * e)
 			__attribute__warn_unused_result__
 			__attribute__nonnull__(1)
 			__attribute__nonnull__(2);
