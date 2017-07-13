073835b69453b23791087e37f660030206d1c18a
Reini Urban <rurban@cpan.org>
2017-07-13 12:11:08 +0200

    method lookup on classes
    also honor inherited classes (restricted stashes)
    in method lookup. Simplify the code for exists/fetch.
    
    TODO: combine HV_FETCH_JUST_SV with HV_FETCH_ISEXISTS to do
    only a single stash lookup, not 2 with classes.

--- gv.c
+++ gv.c
@@ -750,9 +750,21 @@ S_gv_fetchmeth_internal(pTHX_ HV* stash, SV* meth, const char* name, STRLEN len,
     else
         cachestash = stash;
 
+    action = create | HV_FETCH_JUST_SV;
     /* check locally for a real method or a cache entry */
-    gvp = (GV**)hv_common(cachestash, meth, name, len,
-                          hv_utf8, action, NULL, 0);
+    if (UNLIKELY(SvREADONLY(cachestash)))
+        action = HV_FETCH_ISEXISTS;
+    he = (HE*)hv_common(cachestash, meth, name, len,
+                        hv_utf8, action, NULL, 0);
+    if (he) {
+        gvp = UNLIKELY(action == HV_FETCH_ISEXISTS)
+            ? (GV**)hv_common(cachestash, meth, name, len,
+                              hv_utf8, create, NULL, 0)
+            : (GV**)he;
+    }
+    else
+        gvp = NULL;
+
     if (gvp) {
         topgv = *gvp;
       have_gv:
@@ -795,6 +807,7 @@ S_gv_fetchmeth_internal(pTHX_ HV* stash, SV* meth, const char* name, STRLEN len,
     action = HV_FETCH_JUST_SV | HV_FETCH_ISEXISTS;
     items = AvFILLp(linear_av); /* no +1, to skip over self */
     while (items--) {
+        action = HV_FETCH_JUST_SV;
         linear_sv = *linear_svp++;
         assert(linear_sv);
         cstash = gv_stashsv(linear_sv, 0);
@@ -805,8 +818,18 @@ S_gv_fetchmeth_internal(pTHX_ HV* stash, SV* meth, const char* name, STRLEN len,
                            HEKfARG(HvNAME_HEK(stash)));
             continue;
         }
-        gvp = (GV**)hv_common(cstash, meth, name, len,
-                              hv_utf8, action, NULL, 0);
+        else if (UNLIKELY(SvREADONLY(cstash)))
+            action = HV_FETCH_ISEXISTS;
+
+        he = (HE*)hv_common(cstash, meth, name, len,
+                            hv_utf8, action, NULL, 0);
+        if (he) {
+            gvp = UNLIKELY(action == HV_FETCH_ISEXISTS)
+                ? (GV**)hv_common(cstash, meth, name, len,
+                                  hv_utf8, HV_FETCH_JUST_SV, NULL, 0)
+                : (GV**)he;
+        }
+        /*gvp = (GV**)hv_fetch(cstash, name, is_utf8 ? -(I32)len : (I32)len, 0);*/
         if (!gvp) {
             if (len > 1 && HvNAMELEN_get(cstash) == 4) {
                 const char *hvname = HvNAME(cstash); assert(hvname);
