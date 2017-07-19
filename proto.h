dfca9d727dc951d84e22760d97653f11463faee7
Karl Williamson <khw@cpan.org>
2017-07-19 11:34:08 -0600

    locale.c: Change static fcn name
    The new name more closely reflects what it does

--- proto.h
+++ proto.h
@@ -8376,7 +8376,7 @@ STATIC char*	S_stdize_locale(pTHX_ char* locs)
 	assert(locs)
 
 #    if defined(WIN32)
-STATIC char*	S_my_setlocale(pTHX_ int category, const char* locale);
+STATIC char*	S_win32_setlocale(pTHX_ int category, const char* locale);
 #    endif
 #  endif
 #endif
