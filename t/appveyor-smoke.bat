808b5861d32d70879d23d3d75245afd985c5372c
Reini Urban <rurban@cpan.org>
2017-07-22 10:58:15 +0200

    appveyor: mingw via strawberry
    Ditch MSVC deploys as they require a special MSVCRT120.dll runtime
    which we cannot distribute, and everybody else already switched
    over to mingw.
    But keep building with MSVC14 to find regressions.
    
    Switch from MSVC12 to MSVC14 deploys if wanted, currently disabled.
    The runtime is much better there.
    
    See #317
    Eventually switch to mingw deploys.

--- t/appveyor-smoke.bat
+++ t/appveyor-smoke.bat
@@ -30,31 +30,6 @@ rem if "%PLATFORM%" == "X64" exit /b
 rem if "%PLATFORM%" == "x86" set PERL_ENCODE_DEBUG=1
 nmake test CCTYPE=MSVC120 USE_NO_REGISTRY=define || exit 1
 
-rem install on master/relprep/tag
-if "%APPVEYOR_REPO_TAG%" == "true" goto tag
-echo branch %APPVEYOR_REPO_BRANCH%
-if "%APPVEYOR_REPO_BRANCH%" == "master" goto nightly
-if "%APPVEYOR_REPO_BRANCH%" == "smoke/relprep" goto nightly
-if "%APPVEYOR_REPO_BRANCH%" == "cperl-tag-deploy-test" goto nightly
-exit /b
-
-:nightly
-nmake install CCTYPE=MSVC120 USE_NO_REGISTRY=undef
-cd ..
-set BITS=64
-if %PLATFORM% == x86 set BITS=32
-7z a -y -sfx cperl-%APPVEYOR_BUILD_VERSION%-win%BITS%.exe c:\cperl\
-del /s /f /q C:\cperl
-exit /b
-
-:tag
-nmake install CCTYPE=MSVC120 USE_NO_REGISTRY=undef
-cd ..
-set BITS=64
-if %PLATFORM% == x86 set BITS=32
-7z a -y -sfx %APPVEYOR_REPO_TAG_NAME%-win%BITS%.exe c:\cperl\
-del /s /f /q C:\cperl
-
 exit /b
 
 :msvc_14
@@ -63,5 +38,37 @@ call "C:\Program Files (x86)\Microsoft Visual Studio %MSVC_VERSION%.0\VC\vcvarsa
 if "%PLATFORM%" == "X64" ( set "PATH=C:\windows\system32;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\BIN\amd64;C:\Program Files (x86)\Windows Kits\8.1\bin\x64;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;C:\windows;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin;C:\Program Files\7-Zip;" ) ELSE ( set "PATH=C:\windows\system32;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\BIN;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;C:\windows;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\7-Zip;" )
 
 cd win32
-nmake test CCTYPE=MSVC140 USE_NO_REGISTRY=define || exit 1
+nmake test CCTYPE=MSVC140 USE_NO_REGISTRY=undef || exit 1
+
+rem install on master/relprep/maint-/tag
+if "%APPVEYOR_REPO_TAG%" == "true" goto tag
+echo branch %APPVEYOR_REPO_BRANCH%
+if "%APPVEYOR_REPO_BRANCH%" == "maint-5.22c" goto nightly
+if "%APPVEYOR_REPO_BRANCH%" == "maint-5.24c" goto nightly
+if "%APPVEYOR_REPO_BRANCH%" == "maint-5.26c" goto nightly
+if "%APPVEYOR_REPO_BRANCH%" == "master" goto nightly
+if "%APPVEYOR_REPO_BRANCH%" == "smoke/relprep" goto nightly
+if "%APPVEYOR_REPO_BRANCH%" == "smoke/maint-5.22c" goto nightly
+if "%APPVEYOR_REPO_BRANCH%" == "smoke/maint-5.24c" goto nightly
+if "%APPVEYOR_REPO_BRANCH%" == "smoke/maint-5.26c" goto nightly
+if "%APPVEYOR_REPO_BRANCH%" == "cperl-tag-deploy-test" goto nightly
+exit /b
+
+:nightly
+nmake install CCTYPE=MSVC140 USE_NO_REGISTRY=undef
+cd ..
+set BITS=64
+if %PLATFORM% == x86 set BITS=32
+7z a -y -sfx cperl-%APPVEYOR_BUILD_VERSION%-win%BITS%.exe c:\cperl\
+del /s /f /q C:\cperl
+exit /b
+
+:tag
+nmake install CCTYPE=MSVC140 USE_NO_REGISTRY=undef
+cd ..
+set BITS=64
+if %PLATFORM% == x86 set BITS=32
+7z a -y -sfx %APPVEYOR_REPO_TAG_NAME%-win%BITS%.exe c:\cperl\
+del /s /f /q C:\cperl
+
 exit /b
