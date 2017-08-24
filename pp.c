d702f6761b3f9bd82c1479940cdd615de20d1dcc
Tony Cook <tony@develop-help.com>
2017-08-24 15:52:33 +1000

    (perl #131954) don't initialize mark before a possible move of the stack
    (cherry picked from commit 57bd660029d94312ca4eb88993889d981f41b484)

--- pp.c
+++ pp.c
@@ -5640,9 +5640,11 @@ PP(pp_list)
 {
     I32 markidx = POPMARK;
     if (GIMME_V != G_ARRAY) {
-	SV **mark = PL_stack_base + markidx;
+        /* don't initialize mark here, EXTEND() may move the stack */
+        SV **mark;
 	dSP;
         EXTEND(SP, 1);          /* in case no arguments, as in @empty */
+        mark = PL_stack_base + markidx;
 	if (++MARK <= SP)
 	    *MARK = *SP;		/* unwanted list, return last item */
 	else
