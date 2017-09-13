7f126f6d6702956193003f231c9f5f0652bc9031
Nicolas R <atoomic@cpan.org>
2017-09-13 16:52:16 -0600

    Replace multiple 'use vars' by 'our' in regen.
    then run ./regen_perly.pl to update perly files

--- regen/regen_lib.pl
+++ regen/regen_lib.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl -w
 use strict;
-use vars qw($Needs_Write $Verbose @Changed $TAP);
+our (@Changed, $TAP);
 use File::Compare;
 use Symbol;
 
 # Common functions needed by the regen scripts
 
-$Needs_Write = $^O eq 'cygwin' || $^O eq 'os2' || $^O eq 'MSWin32';
+our $Needs_Write = $^O eq 'cygwin' || $^O eq 'os2' || $^O eq 'MSWin32';
 
-$Verbose = 0;
+our $Verbose = 0;
 @ARGV = grep { not($_ eq '-q' and $Verbose = -1) }
   grep { not($_ eq '--tap' and $TAP = 1) }
   grep { not($_ eq '-v' and $Verbose = 1) } @ARGV;
