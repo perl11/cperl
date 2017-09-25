2ae2c340d0752446677ba688fef44023ae289390
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-09-25 09:54:23 +0100

    Update File-Fetch to CPAN version 0.54
      [DELTA]
    
    0.54        Sun Sep 24 19:46:04 2017
    
    * Switch test URL from www.cpan.org to httpbin.org

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -711,7 +711,7 @@ use File::Glob qw(:case);
     },
 
     'File::Fetch' => {
-        'DISTRIBUTION' => 'BINGOS/File-Fetch-0.52.tar.gz',
+        'DISTRIBUTION' => 'BINGOS/File-Fetch-0.54.tar.gz',
         'FILES'        => q[cpan/File-Fetch],
     },
 
