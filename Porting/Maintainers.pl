9c026d37f943eca7a76c31bff8b29073fa177d58
Reini Urban <rurban@cpan.org>
2017-10-15 13:52:12 +0200

    Porting: bumped Storable to 3.05_14

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1291,7 +1291,7 @@ use File::Glob qw(:case);
     },
 
     'Storable' => {
-        'DISTRIBUTION' => 'RURBAN/Storable-3.05_12.tar.gz',
+        'DISTRIBUTION' => 'RURBAN/Storable-3.05_14.tar.gz',
         'FILES'        => q[dist/Storable],
     },
 
