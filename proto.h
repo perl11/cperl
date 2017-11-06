014f32b5711ae95ba52ba0c53b74deb0f90a0625
Karl Williamson <khw@cpan.org>
2017-11-06 14:12:41 -0700

    toke.c: Add limit parameter to 3 static functions
    This will make it possible to fix to handle embedded NULs in the next
    commits.
    
    cperl: also fix missingterm for embedded NULs

--- proto.h
+++ proto.h
@@ -10437,27 +10437,29 @@ STATIC SV*	S_get_and_check_backslash_N_name(pTHX_ const char* s, const char* con
 #define PERL_ARGS_ASSERT_GET_AND_CHECK_BACKSLASH_N_NAME	\
 	assert(s); assert(e)
 
-STATIC void	S_incline(pTHX_ const char *s)
-			__attribute__nonnull__(pTHX_1);
+STATIC void	S_incline(pTHX_ const char *s, const char *end)
+			__attribute__nonnull__(pTHX_1)
+			__attribute__nonnull__(pTHX_2);
 #define PERL_ARGS_ASSERT_INCLINE	\
-	assert(s)
+	assert(s); assert(end)
 
 STATIC int	S_intuit_method(pTHX_ char *s, SV *ioname, CV *cv)
 			__attribute__nonnull__(pTHX_1);
 #define PERL_ARGS_ASSERT_INTUIT_METHOD	\
 	assert(s)
 
-STATIC int	S_intuit_more(pTHX_ char *s)
-			__attribute__nonnull__(pTHX_1);
+STATIC int	S_intuit_more(pTHX_ char *s, char *e)
+			__attribute__nonnull__(pTHX_1)
+			__attribute__nonnull__(pTHX_2);
 #define PERL_ARGS_ASSERT_INTUIT_MORE	\
-	assert(s)
+	assert(s); assert(e)
 
 STATIC I32	S_lop(pTHX_ I32 f, expectation x, char *s)
 			__attribute__nonnull__(pTHX_3);
 #define PERL_ARGS_ASSERT_LOP	\
 	assert(s)
 
-PERL_STATIC_NO_RET void	S_missingterm(pTHX_ char *s)
+PERL_STATIC_NO_RET void	S_missingterm(pTHX_ char *s, STRLEN len)
 			__attribute__noreturn__;
 
 STATIC SV*	S_new_constant(pTHX_ const char *s, STRLEN len, const char *key, STRLEN keylen, SV *sv, SV *pv, const char *type, STRLEN typelen)
