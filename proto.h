cfe181f2d9b402fdafd1ab649e4afdf331433032
Lukas Mai <l.mai@web.de>
2017-11-09 00:59:53 +0100

    add wrap_keyword_plugin function (RT #132413)

--- proto.h
+++ proto.h
@@ -6892,6 +6892,13 @@ PERL_CALLCONV I32	Perl_whichsig_sv(pTHX_ SV* sigsv)
 #define PERL_ARGS_ASSERT_WHICHSIG_SV	\
 	assert(sigsv)
 
+PERL_CALLCONV void	Perl_wrap_keyword_plugin(pTHX_ Perl_keyword_plugin_t new_plugin, Perl_keyword_plugin_t *old_plugin_p)
+			__attribute__global__
+			__attribute__nonnull__(pTHX_1)
+			__attribute__nonnull__(pTHX_2);
+#define PERL_ARGS_ASSERT_WRAP_KEYWORD_PLUGIN	\
+	assert(new_plugin); assert(old_plugin_p)
+
 PERL_CALLCONV void	Perl_wrap_op_checker(pTHX_ Optype opcode, Perl_check_t new_checker, Perl_check_t *old_checker_p)
 			__attribute__global__
 			__attribute__nonnull__(pTHX_2)
