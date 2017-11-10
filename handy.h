4d7175fd086bb36cd2ba695b43e1f3a2d16f5d0d
Karl Williamson <khw@cpan.org>
2017-11-09 18:26:43 -0700

    handy.h: Clarify comment

--- handy.h
+++ handy.h
@@ -264,8 +264,9 @@ typedef long intptr_t
 
 #endif
 
-/* log(2) is pretty close to  0.30103, just in case anyone is grepping for it */
-#define BIT_DIGITS(N)   (((N)*146)/485 + 1)  /* log2(10) =~ 146/485 */
+/* log(2) (i.e., log base 10 of 2) is pretty close to 0.30103, just in case
+ * anyone is grepping for it */
+#define BIT_DIGITS(N)   (((N)*146)/485 + 1)  /* log10(2) =~ 146/485 */
 #define TYPE_DIGITS(T)  BIT_DIGITS(sizeof(T) * 8)
 #define TYPE_CHARS(T)   (TYPE_DIGITS(T) + 2) /* sign, NUL */
 
