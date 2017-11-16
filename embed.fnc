Perl/perl5

Add another param to validate_proto
I need this in order to fix bug #131883.  Since it has a bit of
churn, I’m putting it in a separate commit.

embed.fnc
@@ -2708,7 +2708,8 @@ s	|int	|tokereport	|I32 rv|NN const YYSTYPE* lvalp
 sf	|void	|printbuf	|NN const char *const fmt|NN const char *const s
 #  endif
 #endif
-EXMp	|bool	|validate_proto	|NN SV *name|NULLOK SV *proto|bool warn
+EXMp	|bool	|validate_proto	|NN SV *name|NULLOK SV *proto|bool warn \
+		|bool curstash
 
 #if defined(PERL_IN_UNIVERSAL_C)
 s	|bool	|isa_lookup	|NN HV *stash|NN const char * const name \

perl11/cperl

embed.fnc
@@ -2899,7 +2899,8 @@ s	|int	|tokereport	|I32 rv|NN const YYSTYPE* lvalp
 sf	|void	|printbuf	|NN const char *const fmt|NN const char *const s
 #  endif
 #endif
-EXMp	|bool	|validate_proto	|NN SV *name|NULLOK SV *proto|bool dowarn|bool maybe_sig
+EXMp	|bool	|validate_proto	|NN SV *name|NULLOK SV *proto|bool dowarn \
+				|bool curstash|bool maybe_sig|
 
 #if defined(PERL_IN_UNIVERSAL_C)
 s	|bool	|isa_lookup	|NN HV *stash|NN const char * const name \