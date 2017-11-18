45240e2b8751e3ca68ef173bbc47d5a443d5eff8
Karl Williamson <khw@cpan.org>
2017-11-18 14:05:07 -0700

    Add is_utf8_non_invariant_string()
    This function tells whether or not its argument is a sequence of bytes
    that is legal Perl-extended-UTF-8, and which either requires UTF-8
    (because it contains wide characters) or would have a different
    representation when not under UTF-8.
    
    This paradigm is used in several places in the perl core to decide
    whether to turn on an SV's utf8 flag.  None of those places realized
    that there was a simple way to avoid rescanning the string (though
    perhaps a good C optimizer would).  This commit creates a funtion that
    does this task without the rescan; the next commits will convert to use
    this function.

--- proto.h
+++ proto.h
@@ -7822,6 +7822,14 @@ PERL_CALLCONV void	Perl_Slab_to_rw(pTHX_ OPSLAB *const slab)
 #endif
 #if defined(PERL_CORE) || defined (PERL_EXT)
 #ifndef PERL_NO_INLINE_FUNCTIONS
+PERL_STATIC_INLINE bool	S_is_utf8_non_invariant_string(const U8* const s, STRLEN len)
+			__attribute__warn_unused_result__
+			__attribute__nonnull__(1);
+#define PERL_ARGS_ASSERT_IS_UTF8_NON_INVARIANT_STRING	\
+	assert(s)
+#endif
+
+#ifndef PERL_NO_INLINE_FUNCTIONS
 PERL_STATIC_INLINE STRLEN	S_sv_or_pv_pos_u2b(pTHX_ SV *sv, const char *pv, STRLEN pos, STRLEN *lenp)
 			__attribute__nonnull__(pTHX_1)
 			__attribute__nonnull__(pTHX_2);
