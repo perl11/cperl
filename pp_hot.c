44c3758fdc81b610c97f3501a0e07cfc6bb409ec
Karl Williamson <khw@cpan.org>
2017-11-22 23:10:58 -0700

    pp_multiconcat() Use faster UTF-8 variant counting
    (cherry picked from commit 7d5ed5d0f107110d6b559148f4f68a8dd26aee46)

--- pp_hot.c
+++ pp_hot.c
@@ -876,7 +876,6 @@ PPt(pp_multiconcat, "(:List(:Str)):Str")
          * length on utf8 args (which was only needed to flag non-utf8
          * args in this loop */
         for (svpv_p = svpv_buf; svpv_p < svpv_end; svpv_p++) {
-            char *p;
             SSize_t len, l, extra;
 
             len = svpv_p->len;
@@ -898,10 +897,8 @@ PPt(pp_multiconcat, "(:List(:Str)):Str")
                     slow_concat = TRUE;
                 }
             } else {
-                p = svpv_p->pv;
-                l = len;
-                while (l--)
-                    extra += !UTF8_IS_INVARIANT(*p++);
+                extra = variant_under_utf8_count((U8 *) svpv_p->pv,
+                                                 (U8 *) svpv_p->pv + len);
             }
             if (UNLIKELY(extra)) {
                 grow       += extra;
