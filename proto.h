bdc4e9c999248db13ff9bd5d71202a5eccacda7a
Karl Williamson <khw@cpan.org>
2017-11-26 17:06:44 -0700

    Use is_utf8_invariant_string() more
    Now that this function was changed to do word-at-a time searching in
    commit e17544a60909ed9555c0dad7cd24afc40eb736e7, we can more quickly
    find the first variant byte in a string, if any.  Given that a lot of
    usage of Perl is on ASCII data, it makes sense to try this first before
    any byte-at-a-time processing.
    
    Since Perl can be used on things that are mostly non-ASCII, we give up
    at the first such one, and process the rest of the string byte-by-byte.
    Otherwise we could have a pipeline of finding the next variant quickly,
    but this would only be faster if variants were rare, which I don't feel
    we can be confident about, after finding at least one.

--- proto.h
+++ proto.h
@@ -2541,19 +2541,19 @@ PERL_CALLCONV bool	Perl_isIDFIRST_lazy(pTHX_ const char* p)
 			__attribute__warn_unused_result__
 			__attribute__nonnull__(1); */
 
-/* PERL_CALLCONV bool	is_c9strict_utf8_string_loc(const U8 *s, const STRLEN len, const U8 **ep)
+/* PERL_CALLCONV bool	is_c9strict_utf8_string_loc(const U8 *s, STRLEN len, const U8 **ep)
 			__attribute__global__
 			__attribute__nonnull__(1)
 			__attribute__nonnull__(3); */
 
 #ifndef PERL_NO_INLINE_FUNCTIONS
-PERL_STATIC_INLINE bool	S_is_c9strict_utf8_string_loclen(const U8 *s, const STRLEN len, const U8 **ep, STRLEN *el)
+PERL_STATIC_INLINE bool	S_is_c9strict_utf8_string_loclen(const U8 *s, STRLEN len, const U8 **ep, STRLEN *el)
 			__attribute__nonnull__(1);
 #define PERL_ARGS_ASSERT_IS_C9STRICT_UTF8_STRING_LOCLEN	\
 	assert(s)
 #endif
 
-/* PERL_CALLCONV bool	Perl_is_invariant_string(const U8* const s, const STRLEN len)
+/* PERL_CALLCONV bool	Perl_is_invariant_string(const U8* const s, STRLEN len)
 			__attribute__global__
 			__attribute__warn_unused_result__
 			__attribute__pure__
@@ -2578,13 +2578,13 @@ PERL_STATIC_INLINE bool	S_is_safe_syscall(pTHX_ const char *pv, STRLEN len, cons
 			__attribute__warn_unused_result__
 			__attribute__nonnull__(1); */
 
-/* PERL_CALLCONV bool	is_strict_utf8_string_loc(const U8 *s, const STRLEN len, const U8 **ep)
+/* PERL_CALLCONV bool	is_strict_utf8_string_loc(const U8 *s, STRLEN len, const U8 **ep)
 			__attribute__global__
 			__attribute__nonnull__(1)
 			__attribute__nonnull__(3); */
 
 #ifndef PERL_NO_INLINE_FUNCTIONS
-PERL_STATIC_INLINE bool	S_is_strict_utf8_string_loclen(const U8 *s, const STRLEN len, const U8 **ep, STRLEN *el)
+PERL_STATIC_INLINE bool	S_is_strict_utf8_string_loclen(const U8 *s, STRLEN len, const U8 **ep, STRLEN *el)
 			__attribute__nonnull__(1);
 #define PERL_ARGS_ASSERT_IS_STRICT_UTF8_STRING_LOCLEN	\
 	assert(s)
@@ -2822,16 +2822,16 @@ PERL_CALLCONV bool	Perl_is_utf8_digit(pTHX_ const U8 *p)
 #define PERL_ARGS_ASSERT_IS_UTF8_DIGIT	\
 	assert(p)
 
-/* PERL_CALLCONV bool	is_utf8_fixed_width_buf_flags(const U8 * const s, const STRLEN len, const U32 flags)
+/* PERL_CALLCONV bool	is_utf8_fixed_width_buf_flags(const U8 * const s, STRLEN len, const U32 flags)
 			__attribute__global__
 			__attribute__nonnull__(1); */
 
-/* PERL_CALLCONV bool	is_utf8_fixed_width_buf_loc_flags(const U8 * const s, const STRLEN len, const U8 **ep, const U32 flags)
+/* PERL_CALLCONV bool	is_utf8_fixed_width_buf_loc_flags(const U8 * const s, STRLEN len, const U8 **ep, const U32 flags)
 			__attribute__global__
 			__attribute__nonnull__(1); */
 
 #ifndef PERL_NO_INLINE_FUNCTIONS
-PERL_STATIC_INLINE bool	S_is_utf8_fixed_width_buf_loclen_flags(const U8 * const s, const STRLEN len, const U8 **ep, STRLEN *el, const U32 flags)
+PERL_STATIC_INLINE bool	S_is_utf8_fixed_width_buf_loclen_flags(const U8 * const s, STRLEN len, const U8 **ep, STRLEN *el, const U32 flags)
 			__attribute__nonnull__(1);
 #define PERL_ARGS_ASSERT_IS_UTF8_FIXED_WIDTH_BUF_LOCLEN_FLAGS	\
 	assert(s)
@@ -2952,7 +2952,7 @@ PERL_STATIC_INLINE bool	S_is_utf8_string_flags(const U8 *s, STRLEN len, const U3
 #endif
 
 #ifndef NO_MATHOMS
-PERL_CALLCONV bool	Perl_is_utf8_string_loc(const U8 *s, const STRLEN len, const U8 **ep)
+PERL_CALLCONV bool	Perl_is_utf8_string_loc(const U8 *s, STRLEN len, const U8 **ep)
 			__attribute__global__
 			__attribute__nonnull__(1)
 			__attribute__nonnull__(3);
