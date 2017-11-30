10eaab63893a06825d4d4bdfdafa2a8abe5b0d30
Karl Williamson <khw@cpan.org>
2017-11-30 12:59:39 -0700

    perluniprops/mktables: Fix bad entry
    I spotted this entry in perluniprops recently:
    
       \p{Nko}                 \p{Script_Extensions=Nko} (NOT \p{NKo})
    
    It's saying Nko is not NKo.  But case isn't supposed to matter.  It
    turned out that the bug was doing an eq without first canonicalizing the
    names to account for case differences.  I was expecting there to be more
    entries that were erroneous, but it was just this one.

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -97472,7 +97472,7 @@ static const U8 WB_table[24][24] = {
  * be0f129691d479aa38646e4ca0ec1ee576ae7f75b0300a5624a7fa862fa8abba lib/unicore/extracted/DLineBreak.txt
  * 92449d354d9f6b6f2f97a292ebb59f6344ffdeb83d120d7d23e569c43ba67cd5 lib/unicore/extracted/DNumType.txt
  * e3a319527153b0c6c0c549b40fc6f3a01a7a0dcd6620784391db25901df3b154 lib/unicore/extracted/DNumValues.txt
- * 3c9c8c306ba6d49f5707bdc217a62f798031d87af9f11e8ab272644cec3048a8 lib/unicore/mktables
+ * dd3cf1dfd7a3f1017aedeb852ddcf791b993ecd38c79f856bb23025081c7d131 lib/unicore/mktables
  * 21653d2744fdd071f9ef138c805393901bb9547cf3e777ebf50215a191f986ea lib/unicore/version
  * 913d2f93f3cb6cdf1664db888bf840bc4eb074eef824e082fceda24a9445e60c regen/charset_translations.pl
  * 48418cbf454eb9ef35c73468ed5ef72ad8603490eabe74181ce4fae42ec72579 regen/mk_invlists.pl
