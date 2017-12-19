8bccec4e7c5f6471016a6cd6b38a63b380d4565c
Reini Urban <rurban@cpan.org>
2017-12-19 12:38:20 +0100

    pp_stat: use (IV)PL_statcache.st_ino for < 0
    ino_t st_ino may be unsigned.
    Fixes Coverity CID #180973 Unsigned compared against 0
    An unsigned value can never be negative, so this test will always
    evaluate the same way.
    
    In Perl_pp_stat: An unsigned value can never be less than 0 (CWE-570)
    The warnings suppressions for -Wtautological-compare and -Wtype-limits are not
    needed anymore.

--- pp_sys.c
+++ pp_sys.c
@@ -3006,11 +3006,7 @@ PP(pp_stat)
 	     */
 	    bool neg;
 	    Stat_t s;
-	    CLANG_DIAG_IGNORE(-Wtautological-compare);
-	    GCC_DIAG_IGNORE(-Wtype-limits);
-	    neg = PL_statcache.st_ino < 0;
-	    GCC_DIAG_RESTORE;
-	    CLANG_DIAG_RESTORE;
+	    neg = (IV)PL_statcache.st_ino < 0;
 	    if (neg) {
 		s.st_ino = (IV)PL_statcache.st_ino;
 		if (LIKELY(s.st_ino == PL_statcache.st_ino)) {
