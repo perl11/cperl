6919afea2d92cfd97968975b0ad297226a1082a4
Karl Williamson <khw@cpan.org>
2017-12-19 16:03:57 -0700

    doop.c: Use MIN()
    This is slightly cleaner than hand rolling the min.
    
    (cherry picked from commit 78ba90076f5958f9830fb2559e21420018be4f8d)

--- doop.c
+++ doop.c
@@ -1114,7 +1114,7 @@ Perl_do_vop(pTHX_ I32 optype, SV *sv, SV *left, SV *right)
         len = leftlen;
     }
     else {  /* Neither is UTF-8 */
-        len = leftlen < rightlen ? leftlen : rightlen;
+        len = MIN(leftlen, rightlen);
     }
 
     lensave = len;
