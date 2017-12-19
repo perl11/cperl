45789b740e938697b13a87d8976dedfb1e1d1e7c
Reini Urban <rurban@cpan.org>
2017-12-19 18:41:08 +0100

    XSLoader: check against NULL modlibname
    CopFILE might return NULL also.
    Fixes Coverity CID #165332 Dereference after null check

--- ext/DynaLoader/XSLoader.c
+++ ext/DynaLoader/XSLoader.c
@@ -32,7 +32,7 @@ XS(XS_XSLoader_load) {
         modlibname = OutCopFILE(PL_curcop);
         DLDEBUG(2,PerlIO_printf(Perl_debug_log, "XSLoader::load from caller '%s', '%s'\n",
                 HvNAME(stash), modlibname));
-        if (strEQ(modlibname, "-e"))
+        if (modlibname && strEQ(modlibname, "-e"))
             modlibname = NULL;
     }
     else {
@@ -58,7 +58,7 @@ XS(XS_XSLoader_load) {
     }
     if (!modlibname) {
         modlibname = OutCopFILE(PL_curcop);
-        if (memEQ(modlibname, "(eval ", 6)) /* This catches RT #115808 */
+        if (modlibname && memEQ(modlibname, "(eval ", 6)) /* This catches RT #115808 */
             modlibname = NULL;
     }
     if (!module) {
