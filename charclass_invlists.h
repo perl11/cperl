8842204ecb22141f464b8797a5b3e44421888195
Karl Williamson <khw@cpan.org>
2017-12-23 14:45:02 -0700

    Unicode::UCD.pm Add undocumented internal feature
    This allows charprop() to be called on a Perl-internal-only property
    
    (cherry picked from commit 7b95de1590e1014fd6f742fa5dda8d82b845c736)

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -97404,7 +97404,7 @@ static const U8 WB_table[24][24] = {
 #endif	/* defined(PERL_IN_REGEXEC_C) */
 
 /* Generated from:
- * 901c4fefab4a0d5ec0b393962441285672d8202bbc11177c78a7db99ec8cbdf1 lib/Unicode/UCD.pm
+ * 74e5f3f3d651cee4ff899139095b1ad6da30e8ba1d33df60bfbcb7554ca9f311 lib/Unicode/UCD.pm
  * ff4404ec64f308bdf7714c50f9fdf0d1d0bf3c34db4d0a67e58ef0c6f88e818f lib/unicore/ArabicShaping.txt
  * 292171a0a1c13d7e581e8781eb4cdf248243b1ab267354a63c7a14429dea2740 lib/unicore/BidiBrackets.txt
  * 8f2695cc42989a79a715ab0d2892bd0c998759180cfdfb998674447f48231940 lib/unicore/BidiMirroring.txt
