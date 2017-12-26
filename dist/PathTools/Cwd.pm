9ff0b9570961936267852d4ad836fbe8994fae76
Zefram <zefram@fysh.org>
2017-12-26 07:46:35 +0000

    avoid infinite recursion in _perl_abs_path()
    If _perl_abs_path() couldn't opendir("..") then it would call
    File::Spec->rel2abs, which was liable to call back to Cwd::getcwd() and
    thus to _perl_abs_path(), producing an infinite recursion.  The call to
    File::Spec->rel2abs is of dubious value anyway, so just turn that into
    an error return.  Fixes [perl #132651].
    
    (cherry picked from commit 815d526fdd295b59d805cd4c2871b7a3604247b1)

--- dist/PathTools/Cwd.pm
+++ dist/PathTools/Cwd.pm
@@ -431,9 +431,7 @@ sub _perl_abs_path (str $start='.')
 	local *PARENT;
 	unless (opendir(PARENT, $dotdots))
 	{
-	    # probably a permissions issue.  Try the native command.
-	    require File::Spec;
-	    return File::Spec->rel2abs( $start, _backtick_pwd() );
+	    return undef;
 	}
 	unless (@cst = stat($dotdots))
 	{
