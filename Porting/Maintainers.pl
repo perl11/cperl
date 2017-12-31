8a738989bf3d44c83d984d5384a2404752832f19
Reini Urban <rurban@cpan.org>
2017-12-31 16:36:06 +0100

    JSON::PP: Update to 2.97000_04

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -857,7 +857,7 @@ use File::Glob qw(:case);
     },
 
     'JSON::PP' => {
-        'DISTRIBUTION' => 'MAKAMAKA/JSON-PP-2.27400.tar.gz',
+        'DISTRIBUTION' => 'MAKAMAKA/JSON-PP-2.97000_04.tar.gz',
         'FILES'        => q[cpan/JSON-PP],
         # fallback to Cpanel::JSON::XS, fixed Boolean
         'CUSTOMIZED'   => [ qw( lib/JSON/PP.pm )],
--- cpan/JSON-PP/lib/JSON/PP/Boolean.pm
+++ cpan/JSON-PP/lib/JSON/PP/Boolean.pm
@@ -1,3 +1,29 @@
+package JSON::PP::Boolean;
+
+use strict;
+use overload (
+   "0+"     => sub { ${$_[0]} },
+   "++"     => sub { $_[0] = ${$_[0]} + 1 },
+   "--"     => sub { $_[0] = ${$_[0]} - 1 },
+   '""'     => sub { ${$_[0]} == 1 ? '1' : '0' },
+   'eq'     => sub {
+     my ($obj, $op) = ref ($_[0]) ? ($_[0], $_[1]) : ($_[1], $_[0]);
+     if ($op eq 'true' or $op eq 'false') {
+       return "$obj" eq 'true' ? 'true' eq $op : 'false' eq $op;
+     }
+     else {
+       return $obj ? 1 == $op : 0 == $op;
+     }
+   },
+    fallback => 1,
+);
+
+$JSON::PP::Boolean::VERSION = '2.97000_04';
+
+1;
+
+__END__
+
 =head1 NAME
 
 JSON::PP::Boolean - dummy module providing JSON::PP::Boolean
@@ -11,15 +37,10 @@ JSON::PP::Boolean - dummy module providing JSON::PP::Boolean
 This module exists only to provide overload resolution for Storable and similar modules. See
 L<JSON::PP> for more info about this class.
 
-=cut
-
-use JSON::PP ();
-
-1;
-
 =head1 AUTHOR
 
 This idea is from L<JSON::XS::Boolean> written by Marc Lehmann <schmorp[at]schmorp.de>
+The implementation is from L<Cpanel::JSON::XS::Boolean> from cperl.
 
 =cut
 
