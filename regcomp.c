531a2a5452e6b58b8211e5d55904f36e5e69ba1d
Karl Williamson <khw@cpan.org>
2018-01-04 12:13:23 -0700

    Script runs: was swallowing next char in pattern
    In /(+script_run:foo)bar/, the 'b' was being swallowed without being
    acted upon.  This would affect the '$' to mean match end of string
    
    (cherry picked from commit a867193dd01dd1bb0f32be5761eaf281a3f02341)

--- regcomp.c
+++ regcomp.c
@@ -10865,8 +10865,6 @@ S_reg(pTHX_ RExC_state_t *pRExC_state, I32 paren, I32 *flagp,U32 depth)
                     return NULL;
                 }
 
-                nextchar(pRExC_state);
-
                 return ret;
             }
         }
