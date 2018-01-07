d5aea5d336a54b3735d099fc6e113848f799827b
Reini Urban <rurban@cpan.org>
2018-01-07 13:18:32 +0100

    libnet-3.11
    keeping our Net::Domain and Net::Cmd fixes

--- cpan/libnet/lib/Net/Cmd.pm
+++ cpan/libnet/lib/Net/Cmd.pm
@@ -28,7 +28,7 @@ BEGIN {
   }
 }
 
-our $VERSION = "3.10_01";
+our $VERSION = "3.11_01";
 our @ISA     = qw(Exporter);
 our @EXPORT  = qw(CMD_INFO CMD_OK CMD_MORE CMD_REJECT CMD_ERROR CMD_PENDING);
 
