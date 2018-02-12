c83087ebedd84148d37e3859c315d8e7a3d5e9ae
Tony Cook <tony@develop-help.com>
2018-02-12 11:07:14 +1100

    bump $warnings::VERSION
    since warnings.(pm|pl) was updated in 25ebbc2270
    
    (cherry picked from commit 95634b11ca08a0894253d3b2939f815ef86600c3)

--- lib/warnings.pm
+++ lib/warnings.pm
@@ -9,7 +9,7 @@ BEGIN {
     keys(%warnings::) = 64; # pre-extend the stash
 }
 
-our $VERSION = "1.40";
+our $VERSION = "1.41";
 
 # Verify that we're called correctly so that warnings will work.
 # Can't use Carp, since Carp uses us!
