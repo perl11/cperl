8fd5c3f50796a2e3fee4d84153a4c3228f650721
Karl Williamson <khw@cpan.org>
2018-03-02 12:30:09 -0700

    Bump version of POSIX.pm to 1.83; warnings to 1.42
    These were made using the old version number, and the error was not
    caught, because of the lateness in getting 5.27.9 tagged
    
    (cherry picked from commit d8255c827dc80db97c8439ea38afc130902a7c1e)

--- ext/POSIX/lib/POSIX.pm
+++ ext/POSIX/lib/POSIX.pm
@@ -4,7 +4,7 @@ use warnings;
 
 our ($AUTOLOAD, %SIGRT);
 
-our $VERSION = '1.82_03';
+our $VERSION = '1.83_03';
 our $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
--- lib/warnings.pm
+++ lib/warnings.pm
@@ -9,7 +9,7 @@ BEGIN {
     keys(%warnings::) = 64; # pre-extend the stash
 }
 
-our $VERSION = "1.41";
+our $VERSION = "1.42";
 
 # Verify that we're called correctly so that warnings will work.
 # Can't use Carp, since Carp uses us!
