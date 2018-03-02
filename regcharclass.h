8af0a51d1aebf3df56668c784432ccc06e304674
Karl Williamson <khw@cpan.org>
2018-03-02 19:50:38 +0000

    Unicode::UCD: max code point is now IV_MAX
    Return the correct value when asked.
    
    (cherry picked from commit b2cd5cb1d8b3c8a7a7f033784d5134d2fbd8cad8)
    
    changed to 0x110000 later

--- regcharclass.h
+++ regcharclass.h
@@ -8321,7 +8321,7 @@
 #endif /* H_REGCHARCLASS */
 
 /* Generated from:
- * 69b5ebb58a900be2476eb02ac79a2fed4d377e179a7cc9f9e0463908f3cd17d1 lib/Unicode/UCD.pm
+ * bb10149bd970d3d9b171df8c1534ca2e8a1f587a360172774f236437d935379e lib/Unicode/UCD.pm
  * 47cb62a53beea6d0263e2147331c7e751853c9327225d95bbe2d9e1dc3e1aa44 lib/unicore/ArabicShaping.txt
  * 153f0a100c315f9f3945e78f57137611d36c44b3a975919c499fd403413fede8 lib/unicore/BidiBrackets.txt
  * fbe806975c1bf9fc9960bbaa39ff6290c42c7da8315f9cd459109b024cc1c485 lib/unicore/BidiMirroring.txt
