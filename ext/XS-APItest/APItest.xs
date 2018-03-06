D0x17/perl5

APItest/t/utf8.t: Simplify some tests
The complicated nested loops of tests this commit replaces don't need to
be such.  To test utf8_is_invariant_string, we just need to put a single
variant in each position of a string that spans more than a full word
(since we have full-word lookup now) and includes partial words on either
side.  We set those partial words up to be one byte each less than a
full word.  The code needs to work on strings that don't start on a full
word, and don't end on one, and this commit continues to do that.

ext/XS-APItest/APItest.xs
@@ -6004,16 +6004,27 @@ test_is_utf8_string(char *s, STRLEN len)
     OUTPUT:
         RETVAL
 
+#define WORDCAST            uintmax_t
+#define WORDSIZE            sizeof(WORDCAST)
+
 AV *
-test_is_utf8_invariant_string_loc(char *s, STRLEN offset, STRLEN len)
+test_is_utf8_invariant_string_loc(unsigned char *s, STRLEN offset, STRLEN len)
     PREINIT:
         AV *av;
         const U8 * ep = NULL;
+        WORDCAST * copy;
     CODE:
+        /* 'offset' is number of bytes past a word boundary the testing of 's'
+         * is to start at.  Allocate space that does start at the word
+         * boundary, and copy 's' to the correct offset past it.  Then call the
+         * tested function with that position */
+        Newx(copy, (len + WORDSIZE - 1) / WORDSIZE, WORDCAST);
+        Copy(s, (U8 *) copy + offset, len, U8);
         av = newAV();
-        av_push(av, newSViv(is_utf8_invariant_string_loc((U8 *) s + offset, len, &ep)));
-        av_push(av, newSViv(ep - ((U8 *) s + offset)));
+        av_push(av, newSViv(is_utf8_invariant_string_loc((U8 *) copy + offset, len, &ep)));
+        av_push(av, newSViv(ep - ((U8 *) copy + offset)));
         RETVAL = av;
+        Safefree(copy);
     OUTPUT:
         RETVAL
 

perl11/cperl

(cherry picked from commit 9a3c92872991415ec8cb2977e94b735767207cfa)

ext/XS-APItest/APItest.xs
@@ -6119,16 +6119,26 @@ test_is_utf8_string(char *s, STRLEN len)
     OUTPUT:
         RETVAL
 
+#define WORDSIZE            sizeof(PERL_UINTMAX_T)
+
 AV *
-test_is_utf8_invariant_string_loc(char *s, STRLEN offset, STRLEN len)
+test_is_utf8_invariant_string_loc(unsigned char *s, STRLEN offset, STRLEN len)
     PREINIT:
         AV *av;
         const U8 * ep = NULL;
+        PERL_UINTMAX_T* copy;
     CODE:
+        /* 'offset' is number of bytes past a word boundary the testing of 's'
+         * is to start at.  Allocate space that does start at the word
+         * boundary, and copy 's' to the correct offset past it.  Then call the
+         * tested function with that position */
+        Newx(copy, (len + WORDSIZE - 1) / WORDSIZE, PERL_UINTMAX_T);
+        Copy(s, (U8 *) copy + offset, len, U8);
         av = newAV();
-        av_push(av, newSViv(is_utf8_invariant_string_loc((U8 *) s + offset, len, &ep)));
-        av_push(av, newSViv(ep - ((U8 *) s + offset)));
+        av_push(av, newSViv(is_utf8_invariant_string_loc((U8 *) copy + offset, len, &ep)));
+        av_push(av, newSViv(ep - ((U8 *) copy + offset)));
         RETVAL = av;
+        Safefree(copy);
     OUTPUT:
         RETVAL
 