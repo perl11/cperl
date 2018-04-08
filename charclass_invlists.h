f127668ba5279d5009ea39abd83c57b03ee7491a
Karl Williamson <khw@cpan.org>
2018-04-08 14:40:49 -0600

    mktables: Add tests for t/re/unipropsFOO
    Make sure that a non-binary property doesn't get mistakenly matched in
    \p{}, which is only for binary ones.  There are some ambiguities that
    this test keeps us from falling victim to.
    
    (cherry picked from commit 346f9bfbe12d91fec534d97f05ab9e7f5a8b8230)

--- charclass_invlists.h
+++ charclass_invlists.h
@@ -170711,7 +170711,7 @@ static const U8 WB_table[24][24] = {
  * be0f129691d479aa38646e4ca0ec1ee576ae7f75b0300a5624a7fa862fa8abba lib/unicore/extracted/DLineBreak.txt
  * 92449d354d9f6b6f2f97a292ebb59f6344ffdeb83d120d7d23e569c43ba67cd5 lib/unicore/extracted/DNumType.txt
  * e3a319527153b0c6c0c549b40fc6f3a01a7a0dcd6620784391db25901df3b154 lib/unicore/extracted/DNumValues.txt
- * 663e853430712260a41ae8b0b4998fedf1d06d983ced9049268152ad2168f193 lib/unicore/mktables
+ * 9e34957f1d0029fcced8f007533eff821851258d44b90cbc56f834813e3c2eb9 lib/unicore/mktables
  * 21653d2744fdd071f9ef138c805393901bb9547cf3e777ebf50215a191f986ea lib/unicore/version
  * 913d2f93f3cb6cdf1664db888bf840bc4eb074eef824e082fceda24a9445e60c regen/charset_translations.pl
  * daa548b5abfbf007c8fa611869d6d721c52eb1308d902c445b3edd46d6ba9845 regen/mk_invlists.pl
