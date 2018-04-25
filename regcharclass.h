416ccc2b4cd42cf3b2b387a969d670a8b9911411
Karl Williamson <khw@cpan.org>
2018-04-25 15:07:14 -0600

    Remove qr/\p{_Case_Ignorable}/
    This property is no longer used in the core, nor in cpan, and is marked
    as for core use only, not necessarily stable.  I have kept it around
    because it was work to remove it, but now the revamping of the property
    lookup scheme was causing failures with it, when compiling on early
    Unicode releases.  That could be fixed with extra work, but simply
    removing it also fixes the problem and avoids future maintenance
    costs.
    
    (cherry picked from commit 3f3e5f02fd7170c0d48828479ecbf3fd5333e794)

--- regcharclass.h
+++ regcharclass.h
@@ -8405,7 +8405,7 @@
  * be0f129691d479aa38646e4ca0ec1ee576ae7f75b0300a5624a7fa862fa8abba lib/unicore/extracted/DLineBreak.txt
  * 92449d354d9f6b6f2f97a292ebb59f6344ffdeb83d120d7d23e569c43ba67cd5 lib/unicore/extracted/DNumType.txt
  * e3a319527153b0c6c0c549b40fc6f3a01a7a0dcd6620784391db25901df3b154 lib/unicore/extracted/DNumValues.txt
- * 8d66a32084288128318a3f3ded6b92605d7124fb865c69e314d5f7f1e6e9f6b5 lib/unicore/mktables
+ * 5bf3726eec1acc3326248040a41de059b14cd1a972b1d0a64d03e9b525addc4f lib/unicore/mktables
  * 21653d2744fdd071f9ef138c805393901bb9547cf3e777ebf50215a191f986ea lib/unicore/version
  * 4bb677187a1a64e39d48f2e341b5ecb6c99857e49d7a79cf503bd8a3c709999b regen/charset_translations.pl
  * 65b2807db5cfc7fea90fdfa697d76a4d87feaf8280abd27efda6f3eb3a85f7bf regen/regcharclass.pl
