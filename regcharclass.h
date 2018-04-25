9e4443f07b947f1da70616f5233afc9dd4e218ae
Karl Williamson <khw@cpan.org>
2018-04-25 16:20:50 -0600

    Remove support for qr/\p{_CanonDCIJ}
    This is the third and final obsolete property that is being removed in 3
    sequential commits.  The property is not used in cpan, and is being
    removed as part of the cleanup instigated because another of the 3 would
    require extra code to handle if we were to keep it around.
    
    (cherry picked from commit 0b86b4d14d890a7e38af5f43d56e9c62ab921840)

--- regcharclass.h
+++ regcharclass.h
@@ -8405,7 +8405,7 @@
  * be0f129691d479aa38646e4ca0ec1ee576ae7f75b0300a5624a7fa862fa8abba lib/unicore/extracted/DLineBreak.txt
  * 92449d354d9f6b6f2f97a292ebb59f6344ffdeb83d120d7d23e569c43ba67cd5 lib/unicore/extracted/DNumType.txt
  * e3a319527153b0c6c0c549b40fc6f3a01a7a0dcd6620784391db25901df3b154 lib/unicore/extracted/DNumValues.txt
- * 37231d01fc0d20fc53df69df44547316ba7870a08525c2cf30a2253fd5d88abe lib/unicore/mktables
+ * 2283b4e6e860c8f452a93703c0621b811d42d746e7be9fbac5b23b10afeb3749 lib/unicore/mktables
  * 21653d2744fdd071f9ef138c805393901bb9547cf3e777ebf50215a191f986ea lib/unicore/version
  * 4bb677187a1a64e39d48f2e341b5ecb6c99857e49d7a79cf503bd8a3c709999b regen/charset_translations.pl
  * 65b2807db5cfc7fea90fdfa697d76a4d87feaf8280abd27efda6f3eb3a85f7bf regen/regcharclass.pl
