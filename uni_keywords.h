22666aa04977fbbf1367f15ee954ffd7dc614948
Karl Williamson <khw@cpan.org>
2018-04-26 02:08:53 -0600

    regen/mk_invlists.pl: Create synonyms for perl props
    This allows our code to not have to be so precise as to which alias for
    a property it uses.
    
    (cherry picked from commit 1aefa327470af34c715bc791e45947bcabe2fae5)

--- uni_keywords.h
+++ uni_keywords.h
@@ -6758,6 +6758,6 @@ MPH_VALt match_uniprop( const unsigned char * const key, const U16 key_len ) {
  * 21653d2744fdd071f9ef138c805393901bb9547cf3e777ebf50215a191f986ea lib/unicore/version
  * 4bb677187a1a64e39d48f2e341b5ecb6c99857e49d7a79cf503bd8a3c709999b regen/charset_translations.pl
  * 03e51b0f07beebd5da62ab943899aa4934eee1f792fa27c1fb638c33bf4ac6ea regen/mk_PL_charclass.pl
- * c7411ec9ac79ac8f973337291e94cced51e92ce6e0661f97641880218a4d6788 regen/mk_invlists.pl
+ * c4ea0682ff7c5f56204bfc4ade5049360c01fb59815bdda1d633fdd7cc8480ca regen/mk_invlists.pl
  * 9ff54d4fec30691759e7f0d8680eaac37da287ed9773c10a4506c9b076e4d89c regen/mph.pl
  * ex: set ro: */
