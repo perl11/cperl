Perl/perl5

Use ck_null for ~.
It no longer needs ck_bitop, which it only used before for the
experimental warning that has been removed.

opcode.h
@@ -1487,7 +1487,7 @@ EXT Perl_check_t PL_check[] /* or perlvars.h */
 	Perl_ck_null,		/* not */
 	Perl_ck_bitop,		/* complement */
 	Perl_ck_bitop,		/* ncomplement */
-	Perl_ck_bitop,		/* scomplement */
+	Perl_ck_null,		/* scomplement */
 	Perl_ck_smartmatch,	/* smartmatch */
 	Perl_ck_fun,		/* atan2 */
 	Perl_ck_fun,		/* sin */

perl11/cperl

(cherry picked from commit 90a59ee553fbce0cfcef9ae57ed0705fe279de0a)

opcode.h
@@ -2816,7 +2816,7 @@ EXT Perl_check_t PL_check[] /* or perlvars.h */
 	Perl_ck_null,		/* not */
 	Perl_ck_bitop,		/* complement */
 	Perl_ck_bitop,		/* i_complement */
-	Perl_ck_bitop,		/* s_complement */
+	Perl_ck_null,		/* s_complement */
 	Perl_ck_smartmatch,	/* smartmatch */
 	Perl_ck_fun,		/* atan2 */
 	Perl_ck_fun,		/* sin */