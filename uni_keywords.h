c98ce1a58745a878adbc2877a7c284417dcc322e
Yves Orton <demerphq@gmail.com>
2018-07-02 15:46:54 +0200

    fix issue in regen/mph under 32 bit builds
    the multiplication overflows so perl "helpfully" convers to a float,
    which then makes things go horribly horribly wrong. `use integer` to the rescue
    
    (cherry picked from commit 72196ef94b98987bb277d8bf6db6efaacd624c3c)
    (fixedup regen)

--- uni_keywords.h
+++ uni_keywords.h
@@ -6839,5 +6839,5 @@ MPH_VALt match_uniprop( const unsigned char * const key, const U16 key_len ) {
  * 4bb677187a1a64e39d48f2e341b5ecb6c99857e49d7a79cf503bd8a3c709999b regen/charset_translations.pl
  * 03e51b0f07beebd5da62ab943899aa4934eee1f792fa27c1fb638c33bf4ac6ea regen/mk_PL_charclass.pl
  * 6a5348f2e4030391c89b4694af609a98f8576d5b85d4c063a57cd4bcd616f9f5 regen/mk_invlists.pl
- * 9ff54d4fec30691759e7f0d8680eaac37da287ed9773c10a4506c9b076e4d89c regen/mph.pl
+ * 26c00d30d608c727e1bb719c63507395a4ceb983609122bce2bd9a295fcc2849 regen/mph.pl
  * ex: set ro: */
