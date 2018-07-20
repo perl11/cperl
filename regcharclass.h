63973aff7e917bf2aee917559c3477c122782457
Karl Williamson <khw@cpan.org>
2018-07-20 14:06:57 -0600

    mktables: Comment, white-space
    (cherry picked from commit 82bfada9ea7ae3377ff9bcbc8c580aba181a5c44)

--- regcharclass.h
+++ regcharclass.h
@@ -8414,7 +8414,7 @@
  * be0f129691d479aa38646e4ca0ec1ee576ae7f75b0300a5624a7fa862fa8abba lib/unicore/extracted/DLineBreak.txt
  * 92449d354d9f6b6f2f97a292ebb59f6344ffdeb83d120d7d23e569c43ba67cd5 lib/unicore/extracted/DNumType.txt
  * e3a319527153b0c6c0c549b40fc6f3a01a7a0dcd6620784391db25901df3b154 lib/unicore/extracted/DNumValues.txt
- * 156a8cca687c02847f9b7bc9300fd14e58b08bac613a19fde2c9696689dad4b2 lib/unicore/mktables
+ * 3d306c4dee8ff76f770f3a8523b2e1e303f6bf82005044587af6c16b092fddca lib/unicore/mktables
  * 21653d2744fdd071f9ef138c805393901bb9547cf3e777ebf50215a191f986ea lib/unicore/version
  * 4bb677187a1a64e39d48f2e341b5ecb6c99857e49d7a79cf503bd8a3c709999b regen/charset_translations.pl
  * be2a72d846f63e5dec40cf239dc302deb34675f6ea1e0eef721bc430aa8f3892 regen/regcharclass.pl
