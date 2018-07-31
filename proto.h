733d75a955c1a64632cf6a26640cf828ef34f362
Karl Williamson <khw@cpan.org>
2018-07-30 21:26:32 -0600

    regcomp.c: Change parameter name
    This makes it clearer what variable this length is for
    
    (cherry picked from commit b41284b964ece1eea32f929b5d51dba68a9b0274)

--- proto.h
+++ proto.h
@@ -9979,7 +9979,7 @@ STATIC void	S_parse_lparen_question_flags(pTHX_ RExC_state_t *pRExC_state)
 #define PERL_ARGS_ASSERT_PARSE_LPAREN_QUESTION_FLAGS	\
 	assert(pRExC_state)
 
-PERL_CALLCONV SV *	Perl_parse_uniprop_string(pTHX_ const char * const name, const Size_t len, const bool to_fold, bool * invert)
+PERL_CALLCONV SV *	Perl_parse_uniprop_string(pTHX_ const char * const name, const Size_t name_len, const bool to_fold, bool * invert)
 			__attribute__global__
 			__attribute__nonnull__(pTHX_1)
 			__attribute__nonnull__(pTHX_4);
