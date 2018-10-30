61f25bb8f8343c34e5afe5319a2870da1dea6a6c
Reini Urban <rurban@cpan.org>
2018-10-30 15:47:19 +0100

    EUMM 8.35_07: fix AR_STATIC_ARGS to be arflags
    With lto there's a linking problem without the lto plugin.
    Using D for deterministic builds should also be used.
    Fixes Win32CORE.a on cygwin.

--- dist/Module-CoreList/lib/Module/CoreList.pm
+++ dist/Module-CoreList/lib/Module/CoreList.pm
@@ -18338,7 +18338,10 @@ our %delta :const = (
             'B::Op_private'         => '5.026005',
             'Module::CoreList'      => '5.20190301c',
             'Module::CoreList::Utils'=> '5.20190301c',
-            'Storable'              => '3.12_03',
+            'Cpanel::JSON::XS'      => '4.09',
+            'ExtUtils::MakeMaker'   => '8.35_07',
+            'ExtUtils::MM_Unix'     => '8.35_07',
+            'Storable'              => '3.12_04',
         }
   }
 );
