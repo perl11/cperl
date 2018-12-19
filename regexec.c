54204e66f8fb15d63fe4a0d10957e51628bca43b
Karl Williamson <khw@cpan.org>
2018-12-19 11:21:28 -0700

    Collapse regnode EXACTFU_SS into EXACTFUP
    EXACTFUP was created by the previous commit to handle a problematic case
    in which not all the code points in an EXACTFU node are /i foldable at
    compile time.  Doing so will allow a future commit to use the pre-folded
    EXACTFU nodes (done in a prior commit), saving execution time for the
    common case.  The only problematic code point is the MICRO SIGN.  Most
    patterns don't use this character.
    
    EXACTFU_SS is problematic in a different way.  It contains the sequence
    'ss' which is folded to by LATIN SMALL LETTER SHARP S, but everything in
    it can be pre-folded (unless it also contains a MICRO SIGN).  The reason
    this is problematic is that it is the only non-UTF-8 node where the
    length in folding can change.  To process it at runtime, the more
    general fold equivalence function is used that is capable of handling
    length disparities, but is slower than the functions otherwise used for
    non-UTF-8.
    
    What I've chosen to do for now is to make a single node type for all the
    problematic cases (which at this time means just the two aforementioned
    ones).  If we didn't do this, we'd have to add a third node type for
    patterns that contain both 'ss' and MICRO.  Or artificially split the
    pattern so the two never were in the same node, but we can't do that
    because it can cause bugs in handling multi-character folds.  If more
    special handling is found to be needed, there'd be a combinatorial
    explosion of additional node types to handle all possible combinations.
    
    What this effectively means is that the slower, more general foldEQ
    function is used for portions of patterns containing the MICRO sign when
    the pattern isn't in UTF-8, even though there is no inherent reason to
    do so for non-UTF-8 strings that don't also contain the 'ss' sequence.
    
    (cherry picked from commit 0ea669f4e37ccfbcd5ad708ca625ec17bf22e5b3)

--- regexec.c
+++ regexec.c
@@ -2314,7 +2314,6 @@ S_find_byclass(pTHX_ regexp * prog, const regnode *c, char *s,
         folder = foldEQ_locale;
         goto do_exactf_non_utf8;
 
-    case EXACTFU_SS:
     case EXACTFUP:      /* Problematic even though pattern isn't UTF-8.  Use
                            full functionality normally not done except for
                            UTF-8 */
@@ -4669,7 +4668,6 @@ S_setup_EXACTISH_ST_c1_c2(pTHX_ const regnode * const text_node, int *c1p,
                         assert(! is_utf8_pat);
                         /* FALLTHROUGH */
                     case EXACTFAA:
-                    case EXACTFU_SS:
                     case EXACTFUP:
                     case EXACTFU:
                         c2 = PL_fold_latin1[c1];
@@ -6427,7 +6425,6 @@ S_regmatch(pTHX_ regmatch_info *reginfo, char *startpos, regnode *prog)
 	    fold_utf8_flags = FOLDEQ_S2_ALREADY_FOLDED;
 	    goto do_exactf;
 
-	case EXACTFU_SS:         /*  /\x{df}/iu   */
         case EXACTFUP:          /*  /foo/iu, and something is problematic in
                                     'foo' so can't take shortcuts. */
             assert(! is_utf8_pat);
@@ -6470,7 +6467,6 @@ S_regmatch(pTHX_ regmatch_info *reginfo, char *startpos, regnode *prog)
 
 	    if (   utf8_target
                 || is_utf8_pat
-                || state_num == EXACTFU_SS
                 || state_num == EXACTFUP
                 || (state_num == EXACTFL && IN_UTF8_CTYPE_LOCALE))
             {
@@ -9372,7 +9368,6 @@ S_regrepeat(pTHX_ regexp *prog, char **startposp, const regnode *p,
         }
         /* FALLTHROUGH */
 
-    case EXACTFU_SS:
     case EXACTFUP:
 
       do_exactf: {
