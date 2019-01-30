4fbd0a973666fc44b8044b816ba07ba5080beeee
Karl Williamson <khw@cpan.org>
2019-01-29 22:01:18 -0700

    pp.c: Add missing assert
    The comments say there is an assert, but it wasn't there.
    
    (cherry picked from commit f4cd1cd9e8d271b135a75b4b6fd817fa758c112a)

--- pp.c
+++ pp.c
@@ -4535,6 +4535,7 @@ PP(pp_ucfirst)
 
 		/* Assert tculen is 2 here because the only two characters that
 		 * get to this part of the code have 2-byte UTF-8 equivalents */
+                assert(tculen == 2);
 		*d++ = *tmpbuf;
 		*d++ = *(tmpbuf + 1);
 		s++;	/* We have just processed the 1st char */
