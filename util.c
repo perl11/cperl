5c11549b231618c723b1b5ea61cc1e4d00b811c1
David Mitchell <davem@iabyn.com>
2019-02-19 14:59:24 +0000

    Perl_my_cxt_init: remove unnecessary code
    In the PERL_GLOBAL_STRUCT_PRIVATE variant of this code, it zeroes out
    unused slots of a freshly-realloced PL_my_cxt_list[]. This is not
    necessary, as those slots won't be used until a new index is allocated,
    as which point the new slot will get written to before anything else.
    
    The non-PERL_GLOBAL_STRUCT_PRIVATE variant of this function doesn't have
    this redundant zeroing.
    
    So remove it.
    
    (cherry picked from commit 6a90e9f22120a5e2df51543e0402e2ad3b11017d)

--- util.c
+++ util.c
@@ -5698,8 +5698,6 @@ Perl_my_cxt_init(pTHX_ const char *my_cxt_key, size_t size)
 
     /* make sure the array is big enough */
     if (PL_my_cxt_size <= index) {
-	int old_size = PL_my_cxt_size;
-	int i;
 	if (PL_my_cxt_size) {
             IV new_size = PL_my_cxt_size;
 	    while (new_size <= index)
@@ -5710,9 +5708,6 @@ Perl_my_cxt_init(pTHX_ const char *my_cxt_key, size_t size)
 	else {
 	    PL_my_cxt_size = 16;
 	    Newx(PL_my_cxt_list, PL_my_cxt_size, void *);
-	}
-	for (i = old_size; i < PL_my_cxt_size; i++) {
-	    PL_my_cxt_list[i] = 0;
 	}
     }
     /* newSV() allocates one more than needed */
