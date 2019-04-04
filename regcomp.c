bf6acc374035ed62b0ba7782cd6d60435b5179b5
Karl Williamson <khw@cpan.org>
2019-04-04 13:44:22 -0600

    Partial patch for [perl #133970] Roman::Unicode broken
    Wrap a call out to Perl code with PUSH/POP stacks.
    This gets this module working again, and maybe head off some other smoke
    failures, but there are other things needed, including tests, that I'm
    working on.
    
    (cherry picked from commit 0e85cecf59b303c34704df5b748ef733e3ecf242)

--- regcomp.c
+++ regcomp.c
@@ -23323,6 +23323,7 @@ Perl_parse_uniprop_string(pTHX_
              * for this property in the hash.  So we have the go ahead to
              * expand the definition ourselves. */
 
+            PUSHSTACKi(PERLSI_MAGIC);
             ENTER;
 
             /* Create a temporary placeholder in the hash to detect recursion
@@ -23410,6 +23411,7 @@ Perl_parse_uniprop_string(pTHX_
 
             FREETMPS;
             LEAVE;
+            POPSTACK;
 
             if (prop_definition) {
 
