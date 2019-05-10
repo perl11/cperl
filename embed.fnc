Perl/perl5

add Perl_dup_warnings() and fix leak
The macro DUP_WARNINGS() was doing (approximately)

    new = CopyD(old, malloc(size), size);

which, depending on how the CopyD macro expanded (e.g. on debugging
builds), could result in its arguments being used multiple times, and
thus malloc() being called multiple times, with the result of the
earlier call(s) then leaking.

Fix this by implementing DUP_WARNINGS using a new function,
Perl_dup_warnings() that stores its intermediate values in local vars.

This function isn't performance critical, as its usually only called
once per cop creation at compile time.

embed.fnc
@@ -3279,4 +3279,6 @@ XEop	|void   |dtrace_probe_op   |NN const OP *op
 XEop	|void   |dtrace_probe_phase|enum perl_phase phase
 #endif
 
+XEop	|STRLEN*|dup_warnings	|NN STRLEN* warnings
+
 : ex: set ts=8 sts=4 sw=4 noet:

perl11/cperl

(cherry picked from commit c1e47bad34ce1d9c84ed57c9b8978bcbd5a02e98)
cperl: blead later needed two more commits to fixupo this mess here, which were
still wrong.
- e14197aa126e873114b4bb8628ed7afa87557457 Add missing ARGS_ASSERT - Karl Williamson
- de59f38ed9c2eb88d97eed5f7ade475479bc3248 Fix dup_warnings() - Karl Williamson
They had no idea that pWARN_STD == NULL, so NN for warnings is wrong.
Hence it also has no ARGS_ASSERT macro.
And the doc had a typo.

embed.fnc
@@ -3537,4 +3537,6 @@ Apd	|void	|prep_ffi_sig	|NN CV* cv|const unsigned int num_args \
 				|NN SV** argp|NN void **argvalues
 Apd	|void	|prep_ffi_ret	|NN CV* cv|NN SV** sp|NULLOK char *rvalue
 
+XEop	|STRLEN*|dup_warnings	|NULLOK STRLEN* warnings
+
 : ex: set ts=8 sts=4 sw=4 noet: