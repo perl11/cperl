Perl/perl5

{op, pp, regcomp}.c: fix missing parentheses in 'if' statement

op.c
@@ -10478,7 +10478,7 @@ S_process_special_blocks(pTHX_ I32 floor, const char *const fullname,
 	    return FALSE;
     } else {
 	if (*name == 'E') {
-	    if strEQ(name, "END") {
+	    if (strEQ(name, "END")) {
 		DEBUG_x( dump_sub(gv) );
 		Perl_av_create_and_unshift_one(aTHX_ &PL_endav, MUTABLE_SV(cv));
 	    } else

perl11/cperl

(cherry picked from commit 916db6e8921c4bf39aa0645f0ac7e6f32f168617)
cperl: not really a bug. cpp now sees if ((...)) everywhere. but for
aesthetics and consistency ok.

op.c
@@ -13199,7 +13199,7 @@ S_process_special_blocks(pTHX_ I32 floor, const char *const fullname,
 	    return FALSE;
     } else {
 	if (*name == 'E') {
-	    if strEQc(name, "END") {
+	    if (strEQc(name, "END")) {
 		DEBUG_x( dump_sub(gv) );
 		Perl_av_create_and_unshift_one(aTHX_ &PL_endav, MUTABLE_SV(cv));
 	    } else